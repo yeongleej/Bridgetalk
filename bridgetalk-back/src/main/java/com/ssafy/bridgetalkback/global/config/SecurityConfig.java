889e07b3c5f9c854732f7b101ca532ba1846bd31
sunkyoung <yskkjdh@gmail.com>
2024-04-23 13:27:43 +0900

    [Setting] 전역예외처리 및 테스트 코드 작성을 위한 세팅

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:add9d253e505b08dac68e3eea1c6e479c51525109c9cfee9bbb798052eddf53a
+size 2834
---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/RepositoryTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:588c71afe7948ea0dda2e7191d01810ac500e5a5510d9f622507ca42bcf6d909
+size 311
