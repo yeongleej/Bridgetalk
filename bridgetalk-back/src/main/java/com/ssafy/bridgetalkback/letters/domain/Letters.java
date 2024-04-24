206aa8e93bb1abc0a388b2c3870e5f168a0f8fd1
이지영 <abcd23589534@gmail.com>
2024-04-24 15:16:23 +0900

    [Fix] 생성자 public -> private 변경
    Former-commit-id: a7e5b889b9cef02287c60dbc4103c79856f062e4

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/domain/Letters.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/domain/Letters.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68cead8a3745a0d3187bc283152a2332b80306405ccce078b880515f082da5fb
-size 1501
+oid sha256:1c42f7dc28eca4945eecd8aee94ceea53e6ffeee6b329ec5794876a765de863f
+size 1433
