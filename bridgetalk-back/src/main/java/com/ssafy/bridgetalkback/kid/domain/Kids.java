4826c51b4fe01f2493b27c5ae0f8916cb38cba6d
sunkyoung <yskkjdh@gmail.com>
2024-04-24 11:10:52 +0900

    [Feat] BaseTimeEntity -> BaseEntity로 변경 및 isDeleted 컬럼 추가
    Former-commit-id: beaebb545acdcb267878200cf311c47148bd533a

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kid/domain/Kids.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kid/domain/Kids.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0876f2a182be6ed55e7dd2e9acffe234229ce1de91ca6948321642a65b654e5
-size 1870
+oid sha256:55c6e07c7c929ce2165575cef22627a3caad3c9c717d6cc8bbb6e3b69aac7762
+size 1776
