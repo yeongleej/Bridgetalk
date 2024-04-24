829f5e05c8e5e56b958b23d534c8742571ce74a9
sunkyoung <yskkjdh@gmail.com>
2024-04-25 04:34:58 +0900

    [Test] 부모 회원가입 API 관련 테스트 코드 작성
    Former-commit-id: f3b53449a5088ef1a53fc12b97fee2e7dabcef93

---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/auth/service/AuthServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7834a4cde09adba59b342bef4f1bac4a0dcee7cb03890062543c3ba7e749a809
+size 3388
---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parents/service/ParentsFindServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5472bc9dac06eaf6f93e4f80b58983d1d64c6db4e4520de7c12232d652edcc6
+size 1534
