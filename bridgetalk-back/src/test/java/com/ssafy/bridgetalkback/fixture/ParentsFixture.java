aaea9904e2bbb39778f12cec11cee3f4b7ba2541
sunkyoung <yskkjdh@gmail.com>
2024-04-25 04:33:56 +0900

    [Test] Email VO 및 Password VO 테스트코드 작성
    Former-commit-id: 97946cdbe9923519f764e3c574c75569b5bd6282

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/fixture/ParentsFixture.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/fixture/ParentsFixture.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76b7fcaa728d2e044553b6e7bdce183edaaea9371b1a412c69c395f620edb224
-size 658
+oid sha256:e8265fc9d38881bc4b9341bd04d47971fcfa3f18c437d56cc861c65a1ea81aa1
+size 891
---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parents/domain/PasswordTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67df88acdebb1e98d04a66371a46a3e58157c376617725ea5719691491577f0d
+size 1451
