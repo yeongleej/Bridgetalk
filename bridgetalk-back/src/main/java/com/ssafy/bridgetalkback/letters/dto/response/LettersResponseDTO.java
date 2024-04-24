0a5ba478e11ade1f2fd129ade5174d076ad9aa9a
yeong <abcd23589534@gmail.com>
2024-04-25 02:33:06 +0900

    [Feat] 부모 음성편지 stt service 로직 메소드 추가-1
    Former-commit-id: 8318422d839f810ac8c07274b7e678d7b5d1abe7

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/response/LettersResponseDTO.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/response/LettersResponseDTO.java
@@ -1,2 +1,3 @@
-package com.ssafy.bridgetalkback.letters.dto.response;public record LettersResponseDTO() {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4cfeb4181f9125f84aa0fb2ee14f5863786e8c59091a2e65659c69d230af9f7e
+size 628
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,9 +1,3 @@
-package com.ssafy.bridgetalkback.letters.service;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class LettersService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a262c0b8d02b2c38660359f16a132c7c137eec0687f7e45ab13d8ac62405f4fe
+size 1421
