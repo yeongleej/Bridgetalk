9a76a6e62aead4ff10eaa164279a438297df7ff4
hyun0-25 <lisa1072@skku.edu>
2024-04-24 15:39:45 +0900

    [Feat] Reports 엔티티 작성
    Former-commit-id: c7402ed6fcd8cdd099d2d64535a34d63c6251291

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parent/domain/ParentsRepository.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parent/domain/ParentsRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.bridgetalkback.parent.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ParentsRepository extends JpaRepository<Parents, UUID> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cbf66aac418703146aa52e2c9790763f6cb2c4cea60251774a576b0bc16feb5
+size 212
