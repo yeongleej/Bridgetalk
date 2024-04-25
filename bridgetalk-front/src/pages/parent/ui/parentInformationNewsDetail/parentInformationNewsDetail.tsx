702a45b40c5e9cb11e438d0f176b3391fc4b9be2
조한빈 <whgksqls9999@naver.com>
2024-04-25 17:46:16 +0900

    [feat] 뉴스 디테일 더미 데이터 출력
    Former-commit-id: aaab9c87d8396befee0872297316aa0f513d9b8a

--- bridgetalk-front/src/pages/parent/ui/parentInformationNewsDetail/parentInformationNewsDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNewsDetail/parentInformationNewsDetail.tsx
@@ -1,3 +1,3 @@
-export function ParentInformationNewsDetail() {
-    return <div>뉴스디테일</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f02d6f7a72df9b23bfbccf51d464c8d1f46e730fa61c5c9412956c38db03a11
+size 1068
