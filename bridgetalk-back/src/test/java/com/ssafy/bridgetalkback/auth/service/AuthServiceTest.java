8d8b61b63af4a4826fc9cbd05849294e36b0a5b7
sunkyoung <yskkjdh@gmail.com>
2024-04-26 05:30:20 +0900

    [Test] 부모 로그인/로그아웃 API 관련 테스트코드 작성
    Former-commit-id: 6c64b3acf2b595193d5aa75af3fe9aeac4441ec5

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/auth/service/AuthServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/auth/service/AuthServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ba226d98aa1093adee0d0d2f88fbc0bbd35704c7348c43b08999a9693d5bf52
-size 3400
+oid sha256:870cbdf07a1329e2617d80751538f6b832202ea0eb34b43414e97e52c629fb42
+size 3409
