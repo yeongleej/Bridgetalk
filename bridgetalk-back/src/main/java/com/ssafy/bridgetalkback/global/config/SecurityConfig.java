95858a43d58c7c91a2e36494ed202b1feb85cbb7
sunkyoung <yskkjdh@gmail.com>
2024-04-25 09:55:50 +0900

    [Refactor] 검증 필요없는 url (/api/auth/** -> /api/auth/signup으로 변경)
    Former-commit-id: 763e50f1183cb7627423ff8faeed9c57712a69c0

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1daef7342dff2cf9bcf42ec3fd70a45e6c62da1ea35e8cdf910eca6f66e3d535
-size 2850
+oid sha256:fa91420d594231cb19d995bd5bc073632452641231beb14e0b6de1b01b59b3ad
+size 2854
