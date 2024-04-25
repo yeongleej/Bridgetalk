da5c4e47fdbb6e51de332c41fb1144cc2c1d1a9a
조한빈 <whgksqls9999@naver.com>
2024-04-25 10:50:28 +0900

    [feat] 테스트 페이지 - 부모 페이지 연결
    Former-commit-id: e7ba002fb4ca2d874216ed2b5413c8bfcd221470

--- bridgetalk-front/src/pages/Test/ui/test/Test.tsx
+++ bridgetalk-front/src/pages/Test/ui/test/Test.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a53454b2d53ead0de01426aca1c734de1deb870df0113ce4a35821eea1cd6367
-size 808
+oid sha256:dabc1396c8ea73aa7ea9ef4934730f19a0b3a77158baa00009263a9c3f79399e
+size 867
