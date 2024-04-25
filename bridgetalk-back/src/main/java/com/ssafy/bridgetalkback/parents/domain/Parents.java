a90fb52a11305427ea8ca2466d2d21558ae475d2
sunkyoung <yskkjdh@gmail.com>
2024-04-25 09:56:29 +0900

    [Refactor] Hibernate가 지원하는 모든 환경에서 일관되게 UUID가 생성되도록 @GeneratedValue(generator = "uuid2")으로 수정
    Former-commit-id: 6b359050c46009dcefea27b1ff0a4af29cccea9f

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/domain/Parents.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/domain/Parents.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:534a0032def8d21a10c972c05043624a255911e3ba5f3de9a76e516ff371544e
-size 2073
+oid sha256:7e11321cea5a70eeeaa62838cf27adfdd8cf9b538f96854a81654d61b9195c00
+size 2062
