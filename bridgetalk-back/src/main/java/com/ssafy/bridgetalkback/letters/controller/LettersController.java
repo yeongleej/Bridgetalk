9b83aea4daa229309bf3f7bcae54e211cd501b8b
이지영 <abcd23589534@gmail.com>
2024-04-25 17:10:01 +0900

    [Feat] S3 config, service, test code 추가
    Former-commit-id: cae7b369d543d66f5fee10113f19394cde3c0553

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e04503d6d972756998ea86f85e255bf4c6d7bce18a84b811ae8eeffcc24d7a86
-size 858
+oid sha256:79a6ff61e7380cca6272f2abf21a3f3a3a719f0c5d55103d0ecbfbc4b893ff18
+size 1388
