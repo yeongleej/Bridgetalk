2cdf5eddb55ae14fbf6d7dddbb5c5d72ca171686
sunkyoung <yskkjdh@gmail.com>
2024-04-25 14:50:15 +0900

    [Feat] ParentsRepository 부모 조회 메소드 변경 및 ParentFindService에 해당 메소드 추가 구현
    Former-commit-id: cac2faf7d13d766f51d6ef7a16e11cbe53a75a76

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/service/ParentsFindService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/service/ParentsFindService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eb150107b05aada1418c668f0f6acaa2d02d724f8d042d0be4d6dc607ea0806
-size 976
+oid sha256:672806b57d2761a4fde98215c5da21a8c2bcf0182b2385effe0f3ca0d9740a61
+size 1461
