f24bdee7cbf7200c00a9bd1d84a160bda381be65
hyun0-25 <lisa1072@skku.edu>
2024-04-25 10:14:12 +0900

    [Fix] BaseEntity 추가, 조인컬럼명 오타 수정
    Former-commit-id: 56c0457b6b6536b55046e6153edb2126f494a010

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c045b23058fe2e2c18b5ee5a59f4a28476b14b3af554d45f6a23c6d3f812a3c
-size 1215
+oid sha256:f4f715f5c649701f9015c296faf3f9bdf433e1f36ea1d39a51e5ebe963393448
+size 1289
