4ad52aea26d60e8d423d9f828219fc7b141b612f
sunkyoung <yskkjdh@gmail.com>
2024-04-26 11:07:39 +0900

    [Feat] 누락된 Swagger 설정 다시 추가
    Former-commit-id: af55df2b419fcb073083baa1c03b45a4cfb87df2

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee8cd4a011b56e23632163d734f20334303983f01fd851f97c614ff6dcc6f0cf
-size 2845
+oid sha256:0cd9e060f7d27f2ada0cc948a9f2efe85081384048ef3e0c221d6bca2abf2f5e
+size 2850
