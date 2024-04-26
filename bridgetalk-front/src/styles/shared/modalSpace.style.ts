977597539483ecf0fbd989070e9406b18327a1e5
조한빈 <whgksqls9999@naver.com>
2024-04-26 14:30:18 +0900

    [Feat] 녹음 완료 후 다음 페이지 이동
    Former-commit-id: 81f1fecc171b6eed7144bca1acf7aa9805cd61a7

--- bridgetalk-front/src/styles/shared/modalSpace.style.ts
+++ bridgetalk-front/src/styles/shared/modalSpace.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47bf0c0abafece31fd336c44d5130c5465a60280b6de1b6c1c492d7dfac91fa3
-size 328
+oid sha256:a6665143a591a2b1f245f4e97ef2e4ba244aa784d0b4edca448c4b7882e84351
+size 492
