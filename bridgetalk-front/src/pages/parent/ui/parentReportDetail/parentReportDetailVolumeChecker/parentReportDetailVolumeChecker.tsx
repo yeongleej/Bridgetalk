4e6961c8592836b8555884ed2ef470b9e1931883
조한빈 <whgksqls9999@naver.com>
2024-04-26 15:02:14 +0900

    [Feat] 음량바 구현
    Former-commit-id: 4e31aaf805872bde9d135c53983364617a9b58a9

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailVolumeChecker/parentReportDetailVolumeChecker.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailVolumeChecker/parentReportDetailVolumeChecker.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23542b2accdea394e7be32745d2f5d491d37b473f36f67130e12a5b4b048553c
-size 1093
+oid sha256:27e0fbebc856893a9aa2f46fc509b79e9c4dcee64aa61c6616d6f882d7662453
+size 1238
