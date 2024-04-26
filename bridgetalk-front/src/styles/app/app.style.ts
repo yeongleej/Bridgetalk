9dcd384164b09c8dd510807c95ba8ad079521e0a
조한빈 <whgksqls9999@naver.com>
2024-04-26 17:13:47 +0900

    [fix] 폰트 파일 못찾는 버그 픽스중 -추후 수정 예정
    Former-commit-id: f341a106393c43f39d2c094c226a84d89684ca41

--- bridgetalk-front/src/styles/app/app.style.ts
+++ bridgetalk-front/src/styles/app/app.style.ts
@@ -1,6 +1,3 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f00608f830b2277e73d3b187775bbd45e371c44c0a35c2a4eb5055ef2293cf7
+size 499
