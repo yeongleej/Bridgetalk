66a9f4a7528c826a8ab0e7be353122345b75ca7a
sunkyoung <yskkjdh@gmail.com>
2024-04-26 11:10:29 +0900

    [Feat] /api/auth/signup 시큐리티 적용안되도록 설정
    Former-commit-id: a37096f2f48c3173b5864e086893e5438d634f2e

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cd9e060f7d27f2ada0cc948a9f2efe85081384048ef3e0c221d6bca2abf2f5e
-size 2850
+oid sha256:fd9eed0d4ced94a0460da8f72883ff3296395d62e2257f85157599d6e1303ab3
+size 2886
