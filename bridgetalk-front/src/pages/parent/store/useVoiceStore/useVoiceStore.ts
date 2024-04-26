97483feef2d7e21e501a659867ba180b69ad7e50
조한빈 <whgksqls9999@naver.com>
2024-04-26 10:47:43 +0900

    [Refactor] 오디오 URL Zustand 상태관리 적용
    Former-commit-id: a128487426bdf0a4222dc8874ab9c7291afb22dd

---
+++ bridgetalk-front/src/pages/parent/store/useVoiceStore/useVoiceStore.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08046965efa639a8ed9535b721a17f9bcb2215188c3146761fe206c491bb408c
+size 279
--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f05ba9642ed452022fe2183d540b38d48cf49103c315079ff13211a12a7e6279
-size 2842
+oid sha256:22181f128db8bc09ac83901830524b8cfe3aa1eaeffbc22c55671737ee9ed0ee
+size 2780
