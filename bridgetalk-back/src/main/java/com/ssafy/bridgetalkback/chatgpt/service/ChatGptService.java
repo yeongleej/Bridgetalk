fb5b9278fd487582ea33a6b983cf8f9a88791abd
hyun0-25 <lisa1072@skku.edu>
2024-04-26 10:07:08 +0900

    [Feat] ChatGPT 요약본 번역 관련 메소드 구현
    Former-commit-id: 5b1b971b8c08ae085320fd6fe0d0d8ce999d111b

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc24918518096ec978ec12186086e0e88b4656592e17a3609dadc90c2d21e0b
-size 2465
+oid sha256:d5c0d94acf80a4dd6fce5021b34e1440ca3688682de8a0bc7c78ca75447795cf
+size 3930
