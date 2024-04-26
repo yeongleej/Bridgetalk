6b3e5c0fcb7478d86dba39e3a1cb4ef1aed315e6
조한빈 <whgksqls9999@naver.com>
2024-04-26 16:45:53 +0900

    [Design] 모달 페이드인 구현
    Former-commit-id: dd33f5f751b917b37a5e1f11452efc019d02f4e9

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailVolumeChecker/parentReportDetailVolumeChecker.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailVolumeChecker/parentReportDetailVolumeChecker.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27e0fbebc856893a9aa2f46fc509b79e9c4dcee64aa61c6616d6f882d7662453
-size 1238
+oid sha256:8e8c22a88d83efb4cefbc54ba0b39c33f6b03d748fa0b7dc07213d076972915c
+size 1237
