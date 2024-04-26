1ca515dc8efa021c41650069b01010eed75e73d6
조한빈 <whgksqls9999@naver.com>
2024-04-26 10:39:12 +0900

    [feat] 녹음 기능 구현
    Former-commit-id: 598f858597d3f0a218921d304bc3178ccbedc484

--- bridgetalk-front/src/shared/model/index.ts
+++ bridgetalk-front/src/shared/model/index.ts
@@ -1,3 +1,5 @@
 export * from '@/shared/model/connectAudioStream/connectAudioStream';
 export * from '@/shared/model/generateAudioContext/generateAudioContext';
 export * from '@/shared/model/generateVolumeCheckInterval/generateVolumeCheckInterval';
+export * from '@/shared/model/startRecordVoice/startRecordVoice';
+export * from '@/shared/model/stopRecordVoice/stopRecordVoice';
---
+++ bridgetalk-front/src/shared/model/startRecordVoice/startRecordVoice.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3c0e660e8c0c2bca388a51979b61d32c37d920f0f9fb96324e6185a203ca0e3
+size 863
