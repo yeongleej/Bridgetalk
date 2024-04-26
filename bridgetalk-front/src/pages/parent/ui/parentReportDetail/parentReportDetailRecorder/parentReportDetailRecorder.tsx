af4fb4bb27c199e6ff0933890eab4d3866c11064
조한빈 <whgksqls9999@naver.com>
2024-04-26 10:39:12 +0900

    [feat] 녹음 기능 구현
    Former-commit-id: 983b63972e1b3ea7fe55877724b4b0e94927dc4b

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2dbac8b36cf95accc5a4801de4b981c56985007489be0957c517df25c7adbfd
-size 2763
+oid sha256:f05ba9642ed452022fe2183d540b38d48cf49103c315079ff13211a12a7e6279
+size 2842
--- bridgetalk-front/src/shared/model/index.ts
+++ bridgetalk-front/src/shared/model/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:737f784074e4d63a04fea287a94f73c73df95e12a14d7e03c7388e7d3f662866
-size 232
+oid sha256:96a2e8f55edf78433c46c03cb2ab28a6c186e2e95ba4302dc4154c5df37b051e
+size 362
