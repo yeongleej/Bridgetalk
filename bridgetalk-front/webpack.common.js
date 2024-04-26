f6ef36068034b2210c0f7c944ee90fdf5b383ef6
조한빈 <whgksqls9999@naver.com>
2024-04-26 17:47:57 +0900

    [fix] 폰트 관련 웹팩 설정 수정중
    Former-commit-id: f47c7ec37ae10d8360f0b7752758dd22fac88cf3

--- bridgetalk-front/webpack.common.js
+++ bridgetalk-front/webpack.common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bcf95f7c9110ac60fc68c97fc541a7a65e3cd5527de7029c6a904520720d2e9
-size 4471
+oid sha256:5bf32732a4a867c83fcf58cf6eea80e2079ac0007c4a44a783e94b885ed7ef81
+size 4449
