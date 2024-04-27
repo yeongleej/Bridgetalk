cfa2d38ebd02f10b5edcd2aaf242669f97d6cf7d
방소영 <bjo8244@naver.com>
2024-04-27 18:02:55 +0900

    [Feat] 삭제하지 않은 편지에 대한 PK로 조회하는 서비스 메소드 구현
    Former-commit-id: 04542c7abc36e717e55893c15278671388f34704

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:016805a74190320a2f30ab5f60c9ee5fc61b9ebfc6ddbc7a7cbdad6d0a42f30e
-size 1797
+oid sha256:dbcdcfd2a2eba2e66b34a8cdbf87516292a45236e69be87f6ae185996ddc1376
+size 2773
