da5178f94b68e0f0335200ce8f4c25909f01e98c
방소영 <bjo8244@naver.com>
2024-04-27 18:03:58 +0900

    [Feat] 편지의 번역 텍스트에 대한 음성 데이터 반환 서비스 구현
    Former-commit-id: 0e35f3e7b038efc003ef13bba43d455174253b79

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbcdcfd2a2eba2e66b34a8cdbf87516292a45236e69be87f6ae185996ddc1376
-size 2773
+oid sha256:8ad7139a058a232ba19f13b5a7d9c5d7c90b0bd3f5dfe5d6ed78b64cf14d7bdd
+size 3353
