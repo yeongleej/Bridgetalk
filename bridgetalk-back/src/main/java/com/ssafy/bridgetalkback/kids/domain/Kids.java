9169a4c7399500cb378655e5c54cc9b7af623730
sunkyoung <yskkjdh@gmail.com>
2024-04-29 03:09:23 +0900

    [Feat] 아이 회원가입 시 랜덤이메일 부여하도록 수정
    Former-commit-id: 17156ae69ab7b84090de41a89e8336a74258e698

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kids/domain/Kids.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kids/domain/Kids.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eb5a36d61dc120cd84bb7f415aceed179284df4f1f87a02c146fde9c663d53e
-size 1739
+oid sha256:d1bea1e290c15d414a085065d6dcb11a296673190b2eaa35e7dd2bf625222259
+size 1846
