95982e6b033589991b34c7a5a26ab7a3fdd0966a
hyun0-25 <lisa1072@skku.edu>
2024-04-29 02:11:44 +0900

    [Refactor] api 호출실패 예외 처리 추가
    Former-commit-id: 6c04340129d06d3491d2a8fe0a793ff191a34000

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c0d94acf80a4dd6fce5021b34e1440ca3688682de8a0bc7c78ca75447795cf
-size 3930
+oid sha256:d2459e500fa741a9daaade66d428a5d047562bc41832075dc9c44d9eba52fecc
+size 4247
