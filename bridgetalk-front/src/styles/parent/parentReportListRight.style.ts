4ed38a09082e9aa7110c223cc08a5976fe443649
조한빈 <whgksqls9999@naver.com>
2024-04-29 11:14:20 +0900

    [Design] 분석 리포트 리스트 페이지 CSS - 스크롤바 제외
    Former-commit-id: f069fcbf5343215e2eaca4a10046b5e57ad58ed8

--- bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b742e9537b060111c9bf92448e74ffb1c0bf3ad9d36142067c1b0fe2bce8b3bc
-size 1713
+oid sha256:4804a00cc10db91999e8cc9b319a40ffd47db29d39436c80708ecb952932db1c
+size 2086
