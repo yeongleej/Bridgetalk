2b637368cc50bc0330f4a455fa4a8f13aba5004d
방소영 <bjo8244@naver.com>
2024-04-29 16:54:23 +0900

    [Refactor] tts 실시간 스트리밍 반환으로 변경
    Former-commit-id: bea75e335563f0f25ec8a7e4de5bc5a55312284f

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ad7139a058a232ba19f13b5a7d9c5d7c90b0bd3f5dfe5d6ed78b64cf14d7bdd
-size 3353
+oid sha256:c43d39423c75983ec2275761e08c0154469961f56ba89a5eb6d96461d372e1b2
+size 3405
