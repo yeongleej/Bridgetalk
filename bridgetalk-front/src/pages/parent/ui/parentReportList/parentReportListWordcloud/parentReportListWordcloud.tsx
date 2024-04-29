856765ec511a5586953c651c1a6c09d40a9aafa0
조한빈 <whgksqls9999@naver.com>
2024-04-29 15:28:32 +0900

    [Design] 워드클라우드 구현
    Former-commit-id: 13a7387b532880f0a2a44d38379f9df99818991c

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListWordcloud/parentReportListWordcloud.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListWordcloud/parentReportListWordcloud.tsx
@@ -1,10 +1,3 @@
-import * as S from '@/styles/parent/parentReportListWordcloud.style';
-
-export function ParentReportListWordcloud() {
-    return (
-        <S.Wrapper>
-            <div className="title">키워드 분석</div>
-            <div className="cloud"></div>
-        </S.Wrapper>
-    );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8f969b10dee6f939e5cf1cede8b7a62348527216dde37cf18f74335dd252d33
+size 656
