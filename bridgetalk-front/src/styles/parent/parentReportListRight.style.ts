ecf6b0d535ac2ef7b4235615bdbc5e18d879364e
조한빈 <whgksqls9999@naver.com>
2024-04-29 16:20:35 +0900

    [Design] 리포트 리스트 페이지 글자 그림자 적용
    Former-commit-id: 563aa5f63770d52e5ed8f207e1510d1cf9212607

--- bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cfb158933e0d4eb90b7281cb02413e8093818778ed6f052e94d33c60707e13a
-size 2703
+oid sha256:fae14bb75ca960cc66d7b1067ee43fcfee2f88e0d16c296fc5073bb0a2ecd109
+size 2809
--- bridgetalk-front/src/styles/parent/parentReportListWordcloud.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListWordcloud.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d8be8a758bd17cfcb8485166343478ac558e4eb3f46519fe7ddc6b56f3fc4be
-size 1582
+oid sha256:d78eb2d845ea1761b9ab99212d8438e96879834bdd16302b7b9006b6c2c3e886
+size 2113
