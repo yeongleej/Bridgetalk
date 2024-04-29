c45a4bcbe19367f6a74194e18875fa9cac62c236
조한빈 <whgksqls9999@naver.com>
2024-04-29 16:24:21 +0900

    [Design] 리포트 리스트 아이템 호버, 클릭 애니메이션
    Former-commit-id: 92bd43761c018b955db107648e8d05e80801aa16

--- bridgetalk-front/src/styles/parent/parentReportListItem.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListItem.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ff2fc7f72ebdc3b145a855d4999e495e79538bf685054f0aae4901777707998
-size 1566
+oid sha256:96fd6b78071e5a2483d4e50b9f4fa1ecb31f3076ff8008d5cfff2baccb405b0a
+size 1828
