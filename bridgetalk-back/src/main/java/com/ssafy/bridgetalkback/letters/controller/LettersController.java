13a139b0666ab37822f3044f9d8c03b7b095811f
이지영 <abcd23589534@gmail.com>
2024-04-29 11:26:31 +0900

    [Fix] letters controller 인증 로직 추가
    Former-commit-id: 65254c75bdbca2d7f8ba9e4a32d9fcc9b4028bf5

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e8e02f2afeff20387e10ff9e695060957f61d3944a94be6d56bfb41d63f0121
-size 1391
+oid sha256:fbdceffd8090adec1b59db496bd9ce83271681fbe01e6fbddebeb652b27352a8
+size 1530
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0a7937112d291db117560e5ae4cb4c2cde0b5e34b9cefde7d30b584c0faee8e
-size 4889
+oid sha256:e6ba3430a7f6d59b0aeafe9755bf6b9a0434a5d462e3e6258d75c345bb7d95fa
+size 4964
