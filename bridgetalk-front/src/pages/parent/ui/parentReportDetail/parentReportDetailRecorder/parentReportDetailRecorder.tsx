115f3d75d952214c07cb1c509e98de1953f3f95e
조한빈 <whgksqls9999@naver.com>
2024-04-29 16:38:32 +0900

    [Design] 리포트 상세정보 화면 타이틀 텍스트 외곽선 적용
    Former-commit-id: ab4b28bdacdba970dcf3b4e5af8365f76b0b8d2a

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetailRecorder/parentReportDetailRecorder.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7a430df42eda4545fe0cff7c18440e72b6eac044fa30ad09bc7c4803255c874
-size 2808
+oid sha256:a202d6e9b89df2ae07e11280ac194f4ea3b865109fe721a123ec7ad89ea1ac32
+size 2862
