77cc7a4629378df097b0348d12a7edba3fec48c9
조한빈 <whgksqls9999@naver.com>
2024-04-29 15:37:26 +0900

    [Design] 분석 상세정보 페이지 타이틀 위치 조정
    Former-commit-id: be092afddcda0f59cc301066d961efd25a30f55b

--- bridgetalk-front/src/styles/parent/parentReportDetail.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportDetail.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9645e364d54aa0b06c0b410978cfada73d959df5533d548aee1e58efd6b56263
-size 2471
+oid sha256:bc0c53f1e0a5d1a42938050484f078a7474ee639505c80d3e008a9e3a2a054c4
+size 2474
