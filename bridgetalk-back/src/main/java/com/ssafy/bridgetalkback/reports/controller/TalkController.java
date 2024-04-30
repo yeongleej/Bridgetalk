0864905a19af703f431c403b8592bc3561cb8515
sunkyoung <yskkjdh@gmail.com>
2024-04-30 15:03:43 +0900

    [Feat] 대화 종료 API 구현
    Former-commit-id: 4a7ab6a0449d85633778efbccb933155365e3e5b

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:15ef1763b71fac257550c9c0d8abcc9ead12063c758038bf7d8e9f1873dcc71f
+size 1265
---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad63a668756374f4279140d2db06f7f2bc024f0737d3280991c71e45e0563477
+size 1931
