2b952c618ff8da4ec79651a8564e09f930abe1d1
조한빈 <whgksqls9999@naver.com>
2024-04-30 15:24:55 +0900

    [Fix] File -> Blob 형태로 전송
    Former-commit-id: d26c866dbab52ec6893dcc83f872f37330ae8968

--- bridgetalk-front/src/pages/parent/query/postVoiceBlob/postVoiceBlob.tsx
+++ bridgetalk-front/src/pages/parent/query/postVoiceBlob/postVoiceBlob.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:065f202da963b3bcd77e024e6dc1e28974aaccd5c6f8fe4f94b195e32d73db9d
-size 605
+oid sha256:b229ad70579f198f8cb808beba9972eb6b63f1914a29609d256480ee41132d8c
+size 460
