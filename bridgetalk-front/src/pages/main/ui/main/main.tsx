5d20832d1d92a0f0f706c81e860e22df75db589d
조한빈 <whgksqls9999@naver.com>
2024-04-30 10:59:40 +0900

    [Design] 메인 페이지 배경화면 적용
    Former-commit-id: a934565e87aced2e6502696d3b50e4265377b400

--- bridgetalk-front/src/pages/main/ui/main/main.tsx
+++ bridgetalk-front/src/pages/main/ui/main/main.tsx
@@ -1,3 +1,3 @@
-export function Main() {
-  return <div></div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b464cc820d3f384daa680b0a10f781e4ee32b5dd1a3418e7a7a8a64c9c1ce8a7
+size 359
--- bridgetalk-front/src/pages/main/ui/start/startPage.tsx
+++ bridgetalk-front/src/pages/main/ui/start/startPage.tsx
@@ -1,3 +1,3 @@
-export function StartPage() {
-  return <div></div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e507f9cb1f27031529c0c3b1a30a7d0a646433bf7ab01b0fb892efa10dcc86bd
+size 56
