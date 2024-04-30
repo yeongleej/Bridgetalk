2be24852763936364b383fb3d0e16210a61dc51b
이지영 <abcd23589534@gmail.com>
2024-04-30 13:11:52 +0900

    [Feat] lettersController TestCode 작성 완료
    Former-commit-id: 8cfe57be95b0584d957b4538bd44ca6de8ba0084

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/request/LettersRequestDTO.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/request/LettersRequestDTO.java
@@ -3,8 +3,10 @@ package com.ssafy.bridgetalkback.letters.dto.request;
 import com.ssafy.bridgetalkback.letters.domain.Letters;
 import com.ssafy.bridgetalkback.parents.domain.Parents;
 import com.ssafy.bridgetalkback.reports.domain.Reports;
+import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
+@Builder
 public record LettersRequestDTO(
         Long reportsId,
         MultipartFile lettersFile
