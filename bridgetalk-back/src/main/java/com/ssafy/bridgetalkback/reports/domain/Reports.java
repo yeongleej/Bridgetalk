585ac5e7ef4da5e6ab3e5ecec89908bd830d15de
hyun0-25 <lisa1072@skku.edu>
2024-05-01 17:55:18 +0900

    [Refactor] reports 엔티티 컬럼 추가
    Former-commit-id: 5cdcd2d067066bdc3999f6c7061311601f6a1573

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:796a297de7afa17f6ca3a83faeae9b30ba71d0919dffb566a87514722e9d5bbe
-size 1254
+oid sha256:62fb02ec969708e6183ca996be9b55779f65b5c4beb188a5c14cd7f231c17af7
+size 1992
