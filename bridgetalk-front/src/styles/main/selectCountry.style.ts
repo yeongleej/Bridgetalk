05aab42e08403bda7320378fc91014da0543d7b5
조한빈 <whgksqls9999@naver.com>
2024-05-01 16:47:42 +0900

    [Design] 국가 선택 페이지 CSS
    Former-commit-id: 102da74db34a881c3a0016df5505be527d01247e

--- bridgetalk-front/src/styles/main/selectCountry.style.ts
+++ bridgetalk-front/src/styles/main/selectCountry.style.ts
@@ -1,21 +1,3 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  .title {
-  }
-
-  .countrylist {
-    display: flex;
-    gap: 2svw;
-  }
-
-  .buttons {
-    display: flex;
-    gap: 2svw;
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:05bcbf6772b131652c8694415bfac12ed6af348ac336d4891be2f653daedfda2
+size 1395
