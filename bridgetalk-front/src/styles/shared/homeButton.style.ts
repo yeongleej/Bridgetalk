7dd04a93cb4f4c3f09025be50b15b5fb9a3cd643
조한빈 <whgksqls9999@naver.com>
2024-05-01 10:03:09 +0900

    [Design] 홈버튼 사이즈 조정
    Former-commit-id: 853165716c38812a6fda866f295e0da2b255a9f2

--- bridgetalk-front/src/styles/shared/homeButton.style.ts
+++ bridgetalk-front/src/styles/shared/homeButton.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22febba2af887b7c2fac4a33287581119b6841dc6223089f858a34ac3c5fa5de
+oid sha256:f8c2636aa52208efd0a6ebad8376a255d885bb0267747f559aa2fac0f9714839
 size 209
