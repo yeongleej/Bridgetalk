6445b9cbbba323c8d50181406667d3664a94b2e3
조한빈 <whgksqls9999@naver.com>
2024-05-02 00:01:16 +0900

    [Design] 프로필 추가/수정 페이지 CSS
    Former-commit-id: 0bf24a30b18bfc3c07433d1a05a48f301a922657

--- bridgetalk-front/src/app/appRoutes.tsx
+++ bridgetalk-front/src/app/appRoutes.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e42fd8666c3209faff2916824b1b54e859cf6c083a4a16456c55e1724a8f479a
-size 2150
+oid sha256:8dc855791edfbe83441abc5d7b79b36a236319c108ba3a2548012dbbaf14015f
+size 2381
--- bridgetalk-front/src/styles/main/profilePage.style.ts
+++ bridgetalk-front/src/styles/main/profilePage.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e372fb3766746fa9a62096db4313f7dde703335a59589d3bc40f351784febf2
-size 2075
+oid sha256:024d22e181560bb3f92536947f2ba8af68742e1cb5297c0956a728a16fcae679
+size 2320
