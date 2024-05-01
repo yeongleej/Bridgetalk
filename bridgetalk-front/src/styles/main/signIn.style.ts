ca6f4bd891015f0b80a14a5beb8fbb888b629f7e
조한빈 <whgksqls9999@naver.com>
2024-05-01 18:06:40 +0900

    [Design] 로그인 페이지 CSS
    Former-commit-id: 0478d0b9e159fc575f418e3cafc472a02ca8e814

--- bridgetalk-front/src/styles/main/signIn.style.ts
+++ bridgetalk-front/src/styles/main/signIn.style.ts
@@ -1,15 +1,3 @@
-import styled from 'styled-components';
-import { backButton } from './common.style';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  .email,
-  .password {
-    display: flex;
-    gap: 2svw;
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b13a79ffd2a1f6f6f54d06ba6509c3ff5fe579b59db1119ffef98ab08ddcff76
+size 895
