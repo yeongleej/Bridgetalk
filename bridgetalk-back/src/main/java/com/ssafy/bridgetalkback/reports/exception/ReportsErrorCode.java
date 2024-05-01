01373a2aa983c281e0c3239816f6aa6798a83974
방소영 <bjo8244@naver.com>
2024-05-01 23:30:57 +0900

    [Feat] Reports 생성 및 삭제되지 않은 Reports PK 로 조회 기능 구현
    Former-commit-id: 8178509d5e019191fdd4e01d8941c1588cdef726

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/exception/ReportsErrorCode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:273b7dd731f8802da699ee7c955c651e99bdc9878bbfff473ba609760fe98ae2
+size 513
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/repository/ReportsRepository.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/repository/ReportsRepository.java
@@ -1,10 +1,12 @@
 package com.ssafy.bridgetalkback.reports.repository;
 
-import com.ssafy.bridgetalkback.kids.domain.Kids;
 import com.ssafy.bridgetalkback.reports.domain.Reports;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.Optional;
 
+@Repository
 public interface ReportsRepository extends JpaRepository<Reports, Long> {
+    Optional<Reports> findByReportsIdAndIsDeleted(Long reportsId, int isDeleted);
 }
