c675b86bae3af2a699ee31f6f342850235d7e341
조한빈 <whgksqls9999@naver.com>
2024-05-01 17:05:32 +0900

    [Design] 비밀번호 입력 페이지 사이즈 재조정
    Former-commit-id: 108cce7ad477b69504389fb942dcf025a8636bcd

--- bridgetalk-front/src/styles/main/inputName.style.ts
+++ bridgetalk-front/src/styles/main/inputName.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7989371cf26b260e6ea8a38a5e446b583884a73c1cbc10aacde0c04dc23fcd6d
-size 1146
+oid sha256:85012c1250bd9122044ac6f1f59b5ab8f1b396af9d173c138100e389d3346e77
+size 1150
