72f98f36d13cbde5748faa8302ecff879d12bb17
hyun0-25 <lisa1072@skku.edu>
2024-05-02 01:09:36 +0900

    [Refactor] 레포트 정보 생성 성능개선 - 비동기처리 구현
    Former-commit-id: b69a5d8a7072991fb7dd18eff3d2c8304163da27

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeec4dca0800ae59e7529a813a9e709f6c551d894dfa7d003adfbc91a6f0959c
-size 3754
+oid sha256:c8c35c8ee193ec67a502569bed03134537a62aa61a1b37e31dd933824e56f07b
+size 4603
