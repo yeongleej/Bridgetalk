ea597e1ade322146ed86719d2c9fed747a60c9c1
hyun0-25 <lisa1072@skku.edu>
2024-05-01 18:06:57 +0900

    [Refactor] 에러코드 오타 수정
    Former-commit-id: b19abac1c78df1e0bee703fa2368b27a3be1f8d3

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/exception/ChatGptErrorCode.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/exception/ChatGptErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f17c74ad7a6cd157b8fb87cca0eaa41f1af91b8461a5bb9fd3b247410c235c5
+oid sha256:c8c9adcfe8d387f85c26f1e4e8101736ddde9ee5a300b426ebf967235e52b11f
 size 600
