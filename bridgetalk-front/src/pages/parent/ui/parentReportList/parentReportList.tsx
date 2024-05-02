bf48a655c32b08a2ec43a396e0700e4d058a64d1
조한빈 <whgksqls9999@naver.com>
2024-05-02 09:31:17 +0900

    [Feat] 리포트 리스트 가져오기 함수 호출
    Former-commit-id: 4c58706ebe2224d38c6ec824d6e5886dd7762c77

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportList.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df57c523828cc6bb674d0cb1bc837152f574dd35a19fac2c07949f9d98f4ec7a
-size 953
+oid sha256:b7db33f4936b00496457bf6f89fc0e754fe2be1aa68fa193551fe1bda4dbb684
+size 1294
