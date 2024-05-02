a28ca54d53d91e7168b2647116465f1862201209
조한빈 <whgksqls9999@naver.com>
2024-05-02 11:03:29 +0900

    [Feat] 리포트 디테일 불러오기 테스트 완료
    Former-commit-id: b07d50739a286c83e55449b898970ddace9867fc

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:038bbd27acdaffd5c6991a701b6cc927c2a86c9133dc6c9564c95d2631437d23
-size 2806
+oid sha256:3c08a813614b1b2bac3a2c898a86f55a9ef99d1b863d1008b1e76f64b37dec57
+size 2523
