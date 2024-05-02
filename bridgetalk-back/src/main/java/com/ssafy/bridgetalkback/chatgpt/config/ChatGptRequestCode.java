d43ab4160f3969c5f62abc81861a60c5a5ac75be
sunkyoung <yskkjdh@gmail.com>
2024-05-02 11:23:32 +0900

    [Feat] CharGptRequestCode ANSWER 추가 및 이를 ChatgptService에 반영
    Former-commit-id: ee94cd735b3fd08786122d67926729847c5ba62e

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/config/ChatGptRequestCode.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/config/ChatGptRequestCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b2c1d59476b09d502a19d4e424a9dce5f47baf00b18ce94bffbdb11b717302c
-size 362
+oid sha256:6e809773e4a8f93e3aa868de5ad7e273a40a3c09c2e274ebe1cae7ed0edf01c1
+size 384
