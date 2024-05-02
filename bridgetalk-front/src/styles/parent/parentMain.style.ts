28d7619e0ab34f850a9cbff0e1cab5e435c6fdfe
조한빈 <whgksqls9999@naver.com>
2024-05-02 10:47:44 +0900

    [Design] 부모 메인 화면 CSS
    Former-commit-id: ad02b06e39adebc7e0f230177d34c0f074f1df11

--- bridgetalk-front/src/styles/parent/parentMain.style.ts
+++ bridgetalk-front/src/styles/parent/parentMain.style.ts
@@ -1,23 +1,3 @@
-import styled from 'styled-components';
-import { buttonNormal } from './common.style';
-
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 20svw;
-
-  width: 100svw;
-  height: 100svh;
-
-  .logo {
-    img {
-      width: 40svw;
-    }
-  }
-
-  button {
-    ${buttonNormal}
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:15de4e821472836f159fda535c1e12614a3da382c8790bbe2e409ea9cd80a796
+size 1310
