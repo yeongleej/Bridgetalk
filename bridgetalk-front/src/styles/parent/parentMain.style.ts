d664f05b7c5139693447777a4d65947074756cb3
조한빈 <whgksqls9999@naver.com>
2024-05-02 10:28:20 +0900

    [Design] 부모 페이지 BridgeTalk 로고 삽입:
    Former-commit-id: 0f5f566c7b263373450c3926ba9ff067b074f382

--- bridgetalk-front/src/styles/parent/parentMain.style.ts
+++ bridgetalk-front/src/styles/parent/parentMain.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:601630eb1f414043935626006e8bccd8aad07bb05332a0286193c37f7fa41ed2
-size 489
+oid sha256:97ed6f7959c7d8968d7c77df3d01937271aaa69b64ce0d0b27ca9a53c910d65c
+size 358
