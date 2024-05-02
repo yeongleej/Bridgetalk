38b6ad44e5f44505019e818f0a9f5c3d90d9981c
sunkyoung <yskkjdh@gmail.com>
2024-05-02 12:05:18 +0900

    [Refactor] 메서드명 오타 수정
    Former-commit-id: 2d0792b24599230e6e4632a3438a16623b38255b

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c440a628126b09436e695ca6143024ce9e137e8fd2a70f74199bc47e55053ee
-size 6332
+oid sha256:6fefc2538edb2c704f35b48aca21e7971d87a1a11389ee13a68d3921b46534d7
+size 6396
