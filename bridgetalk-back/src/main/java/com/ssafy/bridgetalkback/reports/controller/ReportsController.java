8ad3f1f3f8a8acb71b3ca5c6df84728865dfeb10
방소영 <bjo8244@naver.com>
2024-05-02 15:35:28 +0900

    [Refactor] Reports 생성 url 수정
    Former-commit-id: a8c7c847b1626c32ffb027e6c294b3b07d04462b

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/ReportsController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/ReportsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f0ae845c84d7030a6df8c2f06f3e4abfa4d4c95f3228263405d35d4789067ed
-size 2284
+oid sha256:8a605597acdaf3696f98df98bb9522920234006ee7c95487bcccc73c19987321
+size 2267
--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/ReportsControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/ReportsControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a8074e2aad50a0ac9ae77a8ccb3103d598d273dd813da0816999570d0e6e367
-size 11444
+oid sha256:32a47558bb5be0b9998719a2374210c17206cb9596305dbf5becebade54aab7a
+size 11390
