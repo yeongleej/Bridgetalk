bf65f093dc258d8e9787de0996675c172d29e397
sunkyoung <yskkjdh@gmail.com>
2024-05-02 15:00:00 +0900

    [Refactor] 오타 수정 및 불필요한 import문 제거
    Former-commit-id: 5826d48eb33a932376b938c59b0f8f4f70262596

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/TalkControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/TalkControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae432f6e438cc6b10e3fec386d49ba1b91edc2ef116964892deceb1b3cb2f47
-size 7667
+oid sha256:64a8fe7ce180285175dc447b5112154f57811354cf89d3afbaf79c1a8bd82108
+size 7548
