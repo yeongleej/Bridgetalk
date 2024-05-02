80fb1ac01fa2d3e9b8ab4641a00af2623860ea6b
방소영 <bjo8244@naver.com>
2024-05-02 15:46:22 +0900

    [Refactor] Dto 생성 정적팩토리메소드로 변경
    Former-commit-id: c5db4c6089d73aff0800f1397e4dac091504ef5e

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2b080558585b07ab879ce2c1ae167fa4ed231d1ba300736eb4c6d1f1438b6c2
-size 3562
+oid sha256:7ca28a8c6706e9f91416ca511b52aadc24685461c04bf1da1125fa64a4880c11
+size 3463
