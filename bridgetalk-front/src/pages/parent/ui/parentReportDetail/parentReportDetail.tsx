6b797d611d505f1f2ea2b68d9c86b62e4c8499c3
조한빈 <whgksqls9999@naver.com>
2024-05-02 10:01:14 +0900

    [Feat] 리포트 디테일 가져오기 reporsId 동적 할당
    Former-commit-id: 5eb33714d2c6815471f33bc77b9d63443d4cd42f

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a582a75b257efcaf070ddb37636ae2febc3b9cfcf237ffaa12459f57092bf86
-size 2646
+oid sha256:038bbd27acdaffd5c6991a701b6cc927c2a86c9133dc6c9564c95d2631437d23
+size 2806
