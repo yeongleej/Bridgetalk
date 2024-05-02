9566d1cc66aac856f5f743f158463b71fca943c6
조한빈 <whgksqls9999@naver.com>
2024-05-03 00:16:20 +0900

    [Feat] 닉네임 유효성 검사
    Former-commit-id: f6f58326b26f897974b1c6ff7634f007e60797d2

--- bridgetalk-front/src/pages/main/model/index.ts
+++ bridgetalk-front/src/pages/main/model/index.ts
@@ -1,3 +1,4 @@
 export * from '@/pages/main/model/handlePasswordCheck/handlePasswordCheck';
 export * from '@/pages/main/model/validatePassword/validatePassword';
 export * from '@/pages/main/model/validateEmail/validateEmail';
+export * from '@/pages/main/model/validateNickname/validateNickname';
