ffe75819e50e4428db63a477d9e4df77d8ac0791
조한빈 <whgksqls9999@naver.com>
2024-05-02 09:44:11 +0900

    [Feat] useReportStore 언어 상태 추가
    Former-commit-id: 06f2dd8d13385175f95d2eda437e9ac26f8e0307

--- bridgetalk-front/src/pages/parent/store/useReportStore/useReportStore.ts
+++ bridgetalk-front/src/pages/parent/store/useReportStore/useReportStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd03a205ff230533f187f8fdbafe0e8207fd374436836ce4bbc1469c169202c1
-size 1343
+oid sha256:02e03599f842df10449c03404e5daba6b20bb5039e6c1a847c9263228929a779
+size 1569
