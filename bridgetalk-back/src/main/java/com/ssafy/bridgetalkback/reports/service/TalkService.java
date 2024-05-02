faf1876a88bd0dfe317a67fc350fa8d43fcf5855
sunkyoung <yskkjdh@gmail.com>
2024-05-02 12:05:41 +0900

    [Feat] TalkService 아이 레포트에 대한 답장 생성 및 tts 변환 메서드 구현
    Former-commit-id: c050fbdc7b45ffb503ff2ec3a68e7ba06f20549c

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ea003570d4cec1b04d41b15ee31f825faf307ee5cfd35e48ec5ad043f6beaf2
-size 1847
+oid sha256:bfe69b1fc236f5081007a5bd384c7c82c020535220fdb7dea837bdf86cff734f
+size 3209
