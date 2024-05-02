d00e83c3c976f253e0a4c138dd906bdddcb5a495
조한빈 <whgksqls9999@naver.com>
2024-05-03 00:12:23 +0900

    [Feat] 이메일 유효성 검사
    Former-commit-id: 07262f4532a81a00c5cf2202b36b24d6d4cfeef9

--- bridgetalk-front/src/pages/main/ui/sign/components/inputName.tsx
+++ bridgetalk-front/src/pages/main/ui/sign/components/inputName.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2db1382d0adb5e9bdea1a8116b98e15eeb748ba680962cf223e227aaf3f30fd
-size 3142
+oid sha256:679fa1e5d61d88731d4426578f7ee3aa4992b8282e7fefac9fb7a4adf6ae903c
+size 3150
