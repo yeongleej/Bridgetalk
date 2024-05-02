82b1a302384cabdb015de07806f971d1e3d4efbd
조한빈 <whgksqls9999@naver.com>
2024-05-02 10:17:53 +0900

    [Feat] 메인 화면 로고 CSS 작업중
    Former-commit-id: d898ace57ae146e78de0c7d08ff63f76024cea31

--- bridgetalk-front/src/styles/parent/parentMain.style.ts
+++ bridgetalk-front/src/styles/parent/parentMain.style.ts
@@ -2,11 +2,28 @@ import styled from 'styled-components';
 import { buttonNormal } from './common.style';
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    gap: 20svw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20svw;
 
-    button {
-        ${buttonNormal}
+  width: 100svw;
+  height: 100svh;
+
+  .logo {
+    /* position: fixed; */
+    top: 10.6svh;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+
+    img {
+      width: 40svw;
+      height: 40svh;
     }
+  }
+  button {
+    ${buttonNormal}
+  }
 `;
