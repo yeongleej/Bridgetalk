ceeffe2e92d785882e820e350023ab2d5c446701
방소영 <bjo8244@naver.com>
2024-05-02 16:10:14 +0900

    [Test] 음성편지 삭제 서비스 호출 시 편지정보가 없을 때 에러코드 확인 테스트 추가
    Former-commit-id: a2e22749715098a771acfeec94f153926d58947c

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/LettersServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/LettersServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed6f77f308b3c7df9bf85249e6835a77347ac4daef5601742e5dfc955f69e5a8
-size 7387
+oid sha256:28b87df4fde3dd375dac4cf5b96ed68d4dddb4b30641d7f35d1f5ecea0bbb534
+size 7689
