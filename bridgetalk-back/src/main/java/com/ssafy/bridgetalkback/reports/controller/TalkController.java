62b50a015c94690f6a941d4a80da35e243fdfc9c
sunkyoung <yskkjdh@gmail.com>
2024-05-02 12:06:45 +0900

    [Feat] TalkController 대화하기 API 구현
    Former-commit-id: 597d89fdadc2a7a6dac755bf1f0a45b7d712ffcc

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2ecf1e11daa539d6bbd191efefac35b2b01f568827a9966577444215aabbfd1
-size 1205
+oid sha256:7e6de75e7296c95372a2210c76fc79b1de8dd66a7a6f355d109acd39405c5529
+size 2138
