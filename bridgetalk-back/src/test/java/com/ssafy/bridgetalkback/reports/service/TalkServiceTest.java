6706504f4b87677eb2cd6f35fd3b8daf0cc43553
sunkyoung <yskkjdh@gmail.com>
2024-05-02 14:03:44 +0900

    [Refactor] 오타 수정
    Former-commit-id: 9570f6cb4288544d5e635e839d71a7cc68ff4769

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/TalkServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/TalkServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c4fa276931ad4c04435c7d857c73f8cccf01902c34e67a69fd4ffe56e2b88d
-size 3118
+oid sha256:76cf2aac77c524e8f3215f3ac43fcff513e94424356e219399e8456dc38bc618
+size 3111
