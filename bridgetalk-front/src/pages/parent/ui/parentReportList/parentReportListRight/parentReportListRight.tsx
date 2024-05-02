3233414d1bc19ad941f3d317dbd149fa8549c917
조한빈 <whgksqls9999@naver.com>
2024-05-02 12:57:09 +0900

    [Feat] 리포트 최신순으로 정렬해서 출력하기
    Former-commit-id: 8cbe5ce97982137e0d7d4aca099096fcf432cc3c

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22aa52c20ee68942bafcfef15554e777c92df2ebeff09951adaa1634e11dd2f6
-size 1898
+oid sha256:5d1bc696f7900f160e1098bf7c8d311bf99c0796fa48a2d9758d8e555b8ade16
+size 2104
