4bf28850efb3b75a7d78507a379f0868d778566e
조한빈 <whgksqls9999@naver.com>
2024-05-02 17:56:49 +0900

    [Feat] 이름 입력 상태관리
    Former-commit-id: 70cf31f659e282a59980215ef9239ca43d8b6fdc

--- bridgetalk-front/src/pages/main/ui/sign/components/inputName.tsx
+++ bridgetalk-front/src/pages/main/ui/sign/components/inputName.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30fa01d4388479afd5538f73b8a64b67f82dccba3477c7a94ff8c85a49922f31
-size 2072
+oid sha256:5a14e1e6b81e6bd6fa8bccdb0d389bc033ff8bea813b45512df9fef1367740c3
+size 2144
