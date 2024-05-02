d4a3141b7f7211ef5bb2f790ec4021491707b0d2
sunkyoung <yskkjdh@gmail.com>
2024-05-02 11:21:10 +0900

    [Refactor] KidsSingupRequestDto -> KidsSignupRequestDto로 네이밍 수정
    Former-commit-id: 94d9a6129a189a3b45413ab369a97a372453772f

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/exception/TranslateBadRequestException.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/exception/TranslateBadRequestException.java
@@ -1,16 +1,3 @@
-package com.ssafy.bridgetalkback.letters.exception;
-
-import java.io.IOException;
-
-public class TranslateBadRequestException extends IOException {
-    private final int statusCode;
-
-    public TranslateBadRequestException(String message, int statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a279bb203aa08476d0f892d610dae5e3c6939cf3998d7f5b9305a4f0be7d582
+size 392
