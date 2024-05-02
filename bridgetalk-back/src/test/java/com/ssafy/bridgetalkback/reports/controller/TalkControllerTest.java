603c0ff11140c69aae18d63fa9cd1b1d8851be55
방소영 <bjo8244@naver.com>
2024-05-02 15:46:53 +0900

    [Test] 대화 시작하기에 대한 유저 검증 로직 테스트
    Former-commit-id: 3cc72b9b62b6743cb85470682230d5d771bb9bac

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/TalkControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/controller/TalkControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9a1be2f3b0393e2dd52c23d02192c62df85255cd63643e8ac853f11defd101
-size 4274
+oid sha256:cfaf48dd07eb649b9ebc56af05565a9794e904da305d4fdaed6a43edf5aafd59
+size 5306
