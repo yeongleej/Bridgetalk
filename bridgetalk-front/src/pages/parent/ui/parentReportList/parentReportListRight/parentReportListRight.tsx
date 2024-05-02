694b97941bd0e94bebbc5715d518f278bbbe27bf
조한빈 <whgksqls9999@naver.com>
2024-05-02 16:14:35 +0900

    [Design] 스크롤바 숨김
    Former-commit-id: 672f60f4cda19a4123f461181bd4f34fcb7096f7

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f8ed5a6812d4ca9107e9de2584b456a502d90c6980cc49346881fc8fb33c472
-size 3075
+oid sha256:6864d8eefaa6229ceb871822998cd50adb7282f0e011e3f6b21770b1eb874600
+size 2691
