4f1b1b8631b3b3285dab40aabbe2b392788451ec
sunkyoung <yskkjdh@gmail.com>
2024-05-03 03:56:16 +0900

    [Feat] Puzzle 엔티티 및 레포지토리 생성
    Former-commit-id: 3e3e1d2536017332e6765a1ae7e8927dd7ab1911

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/puzzle/domain/Puzzle.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:841f94889b4cae37b9a3900829ee560a65bcf558ca49975508c8f4b14f8b4a81
+size 1354
--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,15 +1,3 @@
-import styled from 'styled-components';
-
-export const Background = styled.div`
-  background-image: url('/assets/img/parent_bg.png');
-  background-size: cover;
-  background-position-x: 30%;
-  width: 100svw;
-  height: 100svh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  overflow: hidden;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:743ed0742a0c6cfa82effad445eed8b2226c24a1ef3c00d4d07dac43df09f90c
+size 316
