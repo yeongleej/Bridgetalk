49b52b8bf6ce7040ed07830b4ff025ade03fd664
sunkyoung <yskkjdh@gmail.com>
2024-05-03 03:57:08 +0900

    [Feat] Puzzle 상세 조회 및 리스트 조회 API 구현 관련 테스트코드 작성
    Former-commit-id: f15a935001fe398adb833dd8d016043c8a5ff35f

---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/fixture/PuzzleFixture.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86a94bd1c280513017a42e9fe421beb6a592264989a38e45992a10340fb8db58
+size 882
---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/puzzle/service/PuzzleServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31ace5c9da7ff23fa13c8112658d69b7048c478be5bbca074f860f4859989575
+size 3082
