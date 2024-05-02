f7c297626d5fb77fd8abb9298a516d403c06bd58
방소영 <bjo8244@naver.com>
2024-05-02 15:45:49 +0900

    [Refactor] Dto Record 및 정적팩토리 메소드로 변경
    Former-commit-id: 929eff7a8b58f04ecd8ffdfaa481d66520c03376

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/dto/ReportsCreateResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/dto/ReportsCreateResponseDto.java
@@ -1,16 +1,3 @@
-package com.ssafy.bridgetalkback.reports.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-public class ReportsCreateResponseDto {
-
-    private final Long reportsId;
-
-    @Builder
-    public ReportsCreateResponseDto(Long reportsId) {
-        this.reportsId = reportsId;
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9cf34cada94dfe65be8ca475f5ef395a74bbd9e9b4fe227447657428dee91291
+size 408
