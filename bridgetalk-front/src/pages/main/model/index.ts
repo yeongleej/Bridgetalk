8fa46c1f78e87e6ff0e79d740a6792ac59b0f1d8
조한빈 <whgksqls9999@naver.com>
2024-05-03 00:18:44 +0900

    [Feat] 이름 유효성 검사
    Former-commit-id: 30a9ce1980d53a3e117db50543eb866c6e68c2d5

--- bridgetalk-front/src/pages/main/model/index.ts
+++ bridgetalk-front/src/pages/main/model/index.ts
@@ -2,3 +2,4 @@ export * from '@/pages/main/model/handlePasswordCheck/handlePasswordCheck';
 export * from '@/pages/main/model/validatePassword/validatePassword';
 export * from '@/pages/main/model/validateEmail/validateEmail';
 export * from '@/pages/main/model/validateNickname/validateNickname';
+export * from '@/pages/main/model/validateName/validateName';
