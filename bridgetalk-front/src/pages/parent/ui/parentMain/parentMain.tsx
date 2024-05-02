075db41ba656debead1ec3447934925a4639ea27
조한빈 <whgksqls9999@naver.com>
2024-05-02 11:23:43 +0900

    [Feat] 부모 메인 페이지에서 자녀 정보 파싱하기
    Former-commit-id: 2bf78dcfcabfa86d4d2b37d0cde3873eeceab1cb

--- bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e818151803b0e03e9b6ab5ebfebb94ed7767252c72df8167e295b4ddd3910eb3
-size 709
+oid sha256:4ce2ae59d9edb80eb5010cce4893be6c8f3bd532f69f24350373f9088c1fe2f2
+size 1164
