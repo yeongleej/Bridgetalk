fd8900d4ae6f2266fca3bb0c039a19b53d65d4a2
조한빈 <whgksqls9999@naver.com>
2024-05-03 13:41:38 +0900

    [Feat] 로그인 후 프로필 동적 렌더링
    Former-commit-id: 985794d9b021fc51422fa1c1d0c16a27090fe3ce

--- bridgetalk-front/src/pages/main/query/index.ts
+++ bridgetalk-front/src/pages/main/query/index.ts
@@ -1,2 +1,3 @@
-export * from '@/pages/main/query/postSignin/postSignin';
-export * from '@/pages/main/query/postSignup/postSignup';
+version https://git-lfs.github.com/spec/v1
+oid sha256:64047d7d868470fb74629468abbd594bef0e1c1a4aa75e35d4afeb906fdf9ae9
+size 182
--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce553b135335631787e03d9df3e02125b0b9d5571a417b3ed5f6778d99d8ab7
-size 2278
+oid sha256:662827e3ee3f2b2cb10c3fe2616905550ad2ddf7cac1ef42231b768167f57aaf
+size 2994
