3449997ed2b9188cc8c495b962c60c260c7c2cb9
조한빈 <whgksqls9999@naver.com>
2024-05-03 13:02:04 +0900

    [Feat] 보고서 없음 조건부 렌더링 위치 재조정
    Former-commit-id: 5dc9b45ae110a053c190382401a2278e4c34b5bf

--- bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2aa65bb8ce38dbe8632de2e48f9c1bbdb52e77f79f12e01f2a20a020899b6f9b
-size 2335
+oid sha256:d91145ca6d0a5b7ac9868487fce67afc66613077dc2e07110040ea44b2608522
+size 2455
