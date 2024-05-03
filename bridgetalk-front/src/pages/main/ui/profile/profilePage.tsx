7c1bff2dbe3e2d5f94dff8504ca4c4488b07850d
조한빈 <whgksqls9999@naver.com>
2024-05-03 17:12:53 +0900

    [Feat] 프로필별 페이지 이동
    Former-commit-id: 0b3b04477e57dbbc4256f073af6e85209acc758f

--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc08e75e471d74cc94d354356777d6110533df12791a046eea5cdac62817fda3
-size 2769
+oid sha256:75de79348a3ce15c2ea538fda5ae0852675d8bb713339ec89ef2b4aa39e45cce
+size 3215
