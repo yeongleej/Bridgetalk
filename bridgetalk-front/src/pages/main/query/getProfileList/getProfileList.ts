9ad14eee5fcf26097396645a5b77814235bcf443
조한빈 <whgksqls9999@naver.com>
2024-05-03 17:40:26 +0900

    [Feat] 아이 프로필 접속 후 다시 프로필 화면으로 갔을 때 부모 토큰으로 프로필 리스트 불러오기
    Former-commit-id: 69dd86f5f0a67e96867dfb134cc1e24432c78628

--- bridgetalk-front/src/pages/main/query/getProfileList/getProfileList.ts
+++ bridgetalk-front/src/pages/main/query/getProfileList/getProfileList.ts
@@ -1,8 +1,3 @@
-import { customAxios } from '@/shared';
-
-export async function getProfileList() {
-  return customAxios
-    .get('/profile')
-    .then((res) => res)
-    .catch((err) => console.log(err));
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ec511c8f3bc3d8677c76badb53dd178c4ab20b67a781c5cec3b7284841c92cb
+size 279
--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75de79348a3ce15c2ea538fda5ae0852675d8bb713339ec89ef2b4aa39e45cce
-size 3215
+oid sha256:c4c9695a8fab069566f74ed6374550c9295e457a23afbc36aa9f72f8c802ffe2
+size 3511
