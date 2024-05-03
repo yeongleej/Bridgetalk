2e398fbecc1b3839853f0adb32250f90ec044397
조한빈 <whgksqls9999@naver.com>
2024-05-03 16:54:29 +0900

    [Feat] 로그인시 초기 화면 접근 불가
    Former-commit-id: 5b7e768fc77eeb751ee4fc2da188c838ea1b3076

---
+++ bridgetalk-front/src/pages/main/ui/guard/loginGuard.tsx
@@ -0,0 +1,21 @@
+import { decodeToken } from '@/shared';
+import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  children: ReactNode;
+}
+
+/**
+ * 로그인시 첫 화면을 건너뛰게 하는 가드
+ * @param param0
+ */
+export function LoginGuard({ children }: Props) {
+  const navigate = useNavigate();
+
+  if (decodeToken('access') !== null) {
+    navigate('/profile');
+  }
+
+  return <>{children}</>;
+}
--- bridgetalk-front/src/pages/main/ui/main/main.tsx
+++ bridgetalk-front/src/pages/main/ui/main/main.tsx
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import * as S from '@/styles/main/main.style';
+import { LoginGuard } from '../guard/loginGuard';
+import { decodeToken } from '@/shared';
 
 export function Main() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate('start');
+    if (decodeToken('access') === null) {
+      navigate('/start');
+    } else {
+      navigate('/profile');
+    }
   }, []);
 
   return (
