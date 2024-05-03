e5805dc50183088c4298d766260a07b5818a1023
sunkyoung <yskkjdh@gmail.com>
2024-05-03 10:20:13 +0900

    [Test] Puzzle 등록 API 관련 테스트코드 작성
    Former-commit-id: ec2a17f563209ff13db96a42a04dae8061f8e54a

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/puzzle/service/PuzzleServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/puzzle/service/PuzzleServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31ace5c9da7ff23fa13c8112658d69b7048c478be5bbca074f860f4859989575
-size 3082
+oid sha256:e261cb1c5301f8d411c24bb985ec346584ca831f21c89a86defc93e97387b2fc
+size 4282
