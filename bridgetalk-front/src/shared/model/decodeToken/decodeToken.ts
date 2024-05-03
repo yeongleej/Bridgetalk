066809a691b223cb3be0d74c97def3acf592924d
조한빈 <whgksqls9999@naver.com>
2024-05-03 12:31:35 +0900

    [Feat] 토큰 디코딩
    Former-commit-id: 4a43fd8d3cfb8f889ae3d3182a82c6089a6101e4

--- bridgetalk-front/src/shared/model/decodeToken/decodeToken.ts
+++ bridgetalk-front/src/shared/model/decodeToken/decodeToken.ts
@@ -1,5 +1,5 @@
 export function decodeToken(tokenType: 'access' | 'refresh') {
-  return atob(sessionStorage.getItem(btoa('access' + process.env.REACT_APP_SECURE_CODE))!).split(
+  return atob(sessionStorage.getItem(btoa(`${tokenType}` + process.env.REACT_APP_SECURE_CODE))!).split(
     `${process.env.REACT_APP_SECURE_CODE}`,
   )[0];
 }
