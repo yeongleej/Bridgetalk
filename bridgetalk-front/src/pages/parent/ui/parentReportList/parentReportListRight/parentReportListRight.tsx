6aae0c32b5f70aecb0e2130a63591d02b335f876
조한빈 <whgksqls9999@naver.com>
2024-05-03 12:37:02 +0900

    [Feat] 리포트 없을 때 조건부 렌더링
    Former-commit-id: c732b6c494e76c0b3c68d25c4f0f6b8d686cf36b

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListRight/parentReportListRight.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcbafb4f1c754df5f9e050e9938fd623797fadedaaf90bd472f93615329c03f6
-size 2112
+oid sha256:6dce8dec66de0f661047803e437b48025252c440bdeb5e8f7b4c2414ae778b27
+size 2198
