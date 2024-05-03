a9a44ef79dcfd8ff7382ba2286051367c7cb55e3
조한빈 <whgksqls9999@naver.com>
2024-05-03 17:56:18 +0900

    [Feat] 프로필 리스트 페이지 리스트 불러오기 기능 보완
    Former-commit-id: 2fbdcd3982b93e354124a728c58343bc67ec2828

--- bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
+++ bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05fe598a81a5578a56958e533e8bf8e976536b7b58e4edde92f5c0def75b53c7
-size 1041
+oid sha256:8c9ec24c47c07b32d7ce0f049c5be11a08d52621f5bd5dfb3fcacf73067ac60a
+size 1147
