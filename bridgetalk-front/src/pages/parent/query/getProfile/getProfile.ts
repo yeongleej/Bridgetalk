989c7af37572af542acd600c6fb2d110c496e730
조한빈 <whgksqls9999@naver.com>
2024-05-03 12:16:51 +0900

    [Feat] Bearer 토큰 동적 할당
    Former-commit-id: ccc2012fbb98c0c94182f0559c7ec614398e2b27

---
+++ bridgetalk-front/src/pages/parent/query/getProfile/getProfile.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:677b222ebdd4834b4d7ef300c42e1976fb1c7110b24b5631a90e6e765a9af4bd
+size 614
--- bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2332c11295d9653906541bb89912d1cc37207ebe8e8efb55b7dba93ba9f14853
-size 1627
+oid sha256:f0528bb3d5b023b624f8154bab7af34a4b377f2cb92087b76c9b5123e5d37849
+size 1264
