2a5c2eb79506c5a61797149979543c3e97be497a
박시연 <xion2664@gmail.com>
2024-05-03 14:50:36 +0900

    [Feat] message UI
    Former-commit-id: 1e6bc1c41ba3755029185527f720d32bc34a3193

--- bridgetalk-front/src/styles/child/talk/messageListItem.style.ts
+++ bridgetalk-front/src/styles/child/talk/messageListItem.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66343fa3f4f83f95f90e5d215e6331e290cf19a26337a56c5bb61537b6d6e6b
-size 87
+oid sha256:77b47cd43e2f36f7cc9dbd692ce5991359b05f4c697d2d671809f752478bd16e
+size 515
