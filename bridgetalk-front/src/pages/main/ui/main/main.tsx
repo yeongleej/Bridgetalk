f6fb867777e4e2c7440a77eec389dadfd3c89213
조한빈 <whgksqls9999@naver.com>
2024-05-04 13:04:34 +0900

    [Feat] 로그인 가드 navigate 비동기 처리 추가
    Former-commit-id: 722345d498cffa6c812cc9f428e57447e15a9a06

--- bridgetalk-front/src/pages/main/ui/main/main.tsx
+++ bridgetalk-front/src/pages/main/ui/main/main.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import * as S from '@/styles/main/main.style';
+import { useEffect } from 'react';
 
 export function Main() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/start');
+  }, []);
+
   return (
     <S.Background>
       <Outlet />
