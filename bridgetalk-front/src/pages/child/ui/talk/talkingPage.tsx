38d5c9631b56110fd1b62dae1404f475d4886f8c
조한빈 <whgksqls9999@naver.com>
2024-05-04 20:01:15 +0900

    [Feat] 대화 마치기 버튼 눌러 대화 종료하기
    Former-commit-id: 9cb48482a6e898a3b3e80eccdbf17d7bb1c4d4e1

--- bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:487ac74cc7b5c0d758d82e5b972bf3e2180da3b59cf5906ba9fe1ed27230e5a1
-size 1232
+oid sha256:c7d742fd5b5bf5ca5f523552f9cc1889f44a5156e9b196aa3e8f7f7a45e53eac
+size 1639
