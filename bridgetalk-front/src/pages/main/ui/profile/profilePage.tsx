840934f7c89e51e2463080a931f82b0c9de93c12
조한빈 <whgksqls9999@naver.com>
2024-05-04 12:37:10 +0900

    [Feat] 프로필리스트 로딩 상태 관리
    Former-commit-id: 63465a8b18f163b94090e8570a28e85ce2d50921

--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aaf9d5409a24a780a9773f1721cfaa8f76054ea9cb5203acb6beddc927733c8
-size 3693
+oid sha256:6306f2c9fe5745aa767fe1976fca4e2f414807fd87a2641aeaf7e9c59e842e96
+size 4154
