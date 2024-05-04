22bf93723a29dd9b75aa51c044ed8698c89733b8
조한빈 <whgksqls9999@naver.com>
2024-05-04 16:50:15 +0900

    [Fix] 작은 따옴표 추가
    Former-commit-id: 2f3998054755504ec94393e654cf2d59f1348dad

--- bridgetalk-front/webpack.prod.js
+++ bridgetalk-front/webpack.prod.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eebed98bb2573f0aba34ced5858eec2818672d969675c528882c23b04784b062
-size 224
+oid sha256:84dda4d0b0c5a87603035f7d940e33b86d9c479cf1a1d0f7c583c0f8d0f34098
+size 226
