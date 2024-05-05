263894ff270ac1155fd00627f59b9a07480d5fae
조한빈 <whgksqls9999@naver.com>
2024-05-05 10:14:36 +0900

    [Feat] 클로저 함수로 평균 녹음 음량 계산하기
    Former-commit-id: 794109c7ece4140ba90f5b1bfc018186fecd7098

--- bridgetalk-front/src/pages/child/index.ts
+++ bridgetalk-front/src/pages/child/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb58a745ee85d938195beac4bb0441e276579f9ebdc82d9dff6d994df53bf5cf
-size 148
+oid sha256:69127a234f721b16a964169a3fe472f3dcc5256d9206e970bee20e2d58bc78a2
+size 145
---
+++ bridgetalk-front/src/pages/child/model/getAvgVolume/getAvgVolume.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd2a8293e97a03f4d18798b7127398d068d7dba64936b413e60d3562cb851211
+size 404
