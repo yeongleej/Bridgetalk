1e3b104383d66048beb85d1eefd4861429ed2f71
sunkyoung <yskkjdh@gmail.com>
2024-05-06 02:08:22 +0900

    [Refactor] 오타 수정 profileLogin -> profileId
    Former-commit-id: 7ffb82d21d0a6c90eee0ac540e201bc2594190cd

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/auth/controller/AuthControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/auth/controller/AuthControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec7a4d95c228d2af18528bbe4190c302428eb315cd71b763019ede6cfe752a11
-size 12151
+oid sha256:7854b02e3cbc9741889fb074c650b4a75506f853d3b6115a8ca8c1227e27733c
+size 12145
