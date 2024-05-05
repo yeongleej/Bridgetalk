2de81b396abab694f3f3bca1d6c0dfe33c31f5a2
조한빈 <whgksqls9999@naver.com>
2024-05-05 10:49:24 +0900

    [Feat] 평균 음량의 60% 미만 시 발화 종료로 판단, 2초 뒤 종료 기능
    Former-commit-id: 1969a84925c7f0658a8b081e7884b53bc896a62c

--- bridgetalk-front/src/pages/child/model/getAvgVolume/getAvgVolume.tsx
+++ bridgetalk-front/src/pages/child/model/getAvgVolume/getAvgVolume.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd2a8293e97a03f4d18798b7127398d068d7dba64936b413e60d3562cb851211
-size 404
+oid sha256:59cda60b98b718ddac85ef59aefab1f48451f656095ec896ceb31ce8822e97e8
+size 371
