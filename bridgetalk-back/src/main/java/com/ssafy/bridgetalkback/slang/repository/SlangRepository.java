a716c4f70a082c36226d3b342012eb4826c0332e
방소영 <bjo8244@naver.com>
2024-05-07 08:45:01 +0900

    [Feat] Slang 전체 조회 api 구현
    Former-commit-id: 4e87555fab1713c119ec06bbadddc745eae9dd6e

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/slang/repository/SlangRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42d77699f6727cea12ffde149418b057c2b40a3dee867bfaed2cdf421117c928
+size 301
---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/slang/service/SlangService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90ea5c9bcb60e0d07f338eeb186b8eadd23e3e23bd3b6af265ca460b62e52207
+size 323
