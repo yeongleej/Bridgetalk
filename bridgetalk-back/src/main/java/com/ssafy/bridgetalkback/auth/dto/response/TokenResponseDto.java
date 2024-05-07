0aa1ef36edf07ec666974fb6e3fcc33c49362584
조한빈 <whgksqls9999@naver.com>
2024-05-07 09:49:54 +0900

    [Feat] getUUIDbyToken 공통 함수 작성
    Former-commit-id: 975365d7118193d5540fbeee7ad463b8ce678c12

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/response/TokenResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/response/TokenResponseDto.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:098f0b05c1d472c69d205f4f466f3faec5627958974bf0ce47fa2e550ca3a5b7
-size 147
+package com.ssafy.bridgetalkback.auth.dto.response;
+
+public record TokenResponseDto(
+        String accessToken,
+        String refreshToken
+) {
+}
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/dto/response/ProfileListResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/dto/response/ProfileListResponseDto.java
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:34e7262b1c8300ed04dc8486a64f8309c01a144f6dfc5687d8ca871cb05e59f4
-size 169
+package com.ssafy.bridgetalkback.parents.dto.response;
+
+import java.util.List;
+
+public record ProfileListResponseDto(
+        List<ProfileResponseDto> profileList
+) {
+}
