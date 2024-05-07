9fbe723abb38437e659c79df0c89cef588d5c3f2
방소영 <bjo8244@naver.com>
2024-05-07 11:15:55 +0900

    [Test] 삭제되지 않은 Slang 전체 조회 api Controller 테스트
    1. 토큰 관련 인증 처리 테스트 성공
    2. 전체 조회 성공
    
    
    Former-commit-id: b0eddda9ad9629f372cea95619d53530c3ed5723

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a670c3b5b1ee84634d220aa13280efb798f93e8c8872d55c3873f0b3a0aa6f
-size 4174
+oid sha256:d2a59d6576913d0908e494a7d1f762c166f2b4b03091396411056afd3b42e35c
+size 4370
