c6b2e6106de14bcc0d21d0a626954c4e824d0489
방소영 <bjo8244@naver.com>
2024-05-07 21:16:17 +0900

    [Test] Slang 전체 조회에 대한 로컬캐시 처리 테스트
    1. 2번 호출 후 반환 속도 비교해서 테스트 성공
    
    
    Former-commit-id: 19978ab85e69d81c6fe893165df7e09e038016ef

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fed115caa323a170c9c1a842c72a7fb49be85385898078456cebd78d30beba5
-size 1369
+oid sha256:80f5b61eeea470be123a8c6476d364d2f039d3d36b1c56972a964f67582d52be
+size 1498
--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/slang/controller/SlangControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/slang/controller/SlangControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f734ddd5f5da277bfa2c0435476ac0ca0f0eb4b5c38d978db1ec571ef29b280
-size 6724
+oid sha256:bcee1af7294968bdfb4ade3f9888f5c3bc3ddaf3dc8954e361a21581352cdc8b
+size 6692
