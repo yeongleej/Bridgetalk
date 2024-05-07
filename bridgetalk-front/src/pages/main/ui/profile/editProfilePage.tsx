17d0406df74ce9b14d17b2ff4a39168cdbbaa126
조한빈 <whgksqls9999@naver.com>
2024-05-07 17:48:39 +0900

    [Fix] 공룡 변경 후 변경 전 데이터 받아오는 버그 픽스
    Former-commit-id: 43b7bccd5e7b153b4f92b9e03823567e068eba46

--- bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:643e68e079c5629be7c63d68876159d7bac764491adc3d20a0e1c2057e6cbcdf
-size 5178
+oid sha256:61f9b7eb25f7025a2814714c518752889f93bd72e08d320f6d914ff45625bfac
+size 5144
