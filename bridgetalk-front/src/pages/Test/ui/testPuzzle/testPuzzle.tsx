1b7352f3782269a275e557c02281164cf54a22f6
박시연 <xion2664@gmail.com>
2024-05-08 15:09:49 +0900

    [Feat] Puzzle
    Former-commit-id: a5b3b017c6d5b76c47effb6fef56ed562344eb45

--- bridgetalk-front/src/pages/Test/ui/testPuzzle/testPuzzle.tsx
+++ bridgetalk-front/src/pages/Test/ui/testPuzzle/testPuzzle.tsx
@@ -1,15 +1,3 @@
-import styled from 'styled-components';
-
-export function TestPuzzle() {
-    return (
-        <Wrapper>
-            <div></div>
-        </Wrapper>
-    );
-}
-
-const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    border: 2px solid red;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:10dd585bd14f5175ce760f0e4a347c734e5b1c60a8e8875c13b25c6e3aa39226
+size 275
--- bridgetalk-front/src/pages/child/store/puzzle/usePuzzleStore.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2cbda087ec85838f15e0dd5b5b2a3e02eccb93fca8a3680c9f671af521fee2ea
-size 390
