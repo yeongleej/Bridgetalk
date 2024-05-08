91f45df1212e608a7fe8b6716b2257bfb6ab8ba6
조한빈 <whgksqls9999@naver.com>
2024-05-08 15:46:59 +0900

    [Docs] public 폴더 내 FFmpeg load시 필요 파일 저장
    Former-commit-id: 483ed7b37933a522426f92dba627018b87d3d447

--- bridgetalk-front/public/@ffmpeg/core/dist/esm/ffmpeg-core.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c972f5abeafcd5f3949e54edfbdc74a9badb27025809feb1945d468ffbcfb7f1
-size 114494
--- bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
+++ bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08f0729650f80e6f5da1056264b8f38b1f60409dcba8b36120904f3db44873c1
-size 316
+oid sha256:1283587bee8bae8252da1803f991cfcaf020e29fefc18c49d39413d560f0cc45
+size 322
