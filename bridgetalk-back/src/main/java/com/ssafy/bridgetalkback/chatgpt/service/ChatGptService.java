ad2f6bd8b3696ca09a8abfde13800dc58d3998b7
hyun0-25 <lisa1072@skku.edu>
2024-05-08 23:19:20 +0900

    [Feat] 레포트 솔루션 기능 구현
    Former-commit-id: 389d079fd86a1bc38535da696c9bfd392cb61e8f

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c805594ef3a42e81f837e993864839180bc9d3a679a354b99f408b491521aaf5
-size 4852
+oid sha256:dafe7cac78494c8949db72a8770511724dc7310eb76d6b0ba69d25c74ca53f17
+size 5461
