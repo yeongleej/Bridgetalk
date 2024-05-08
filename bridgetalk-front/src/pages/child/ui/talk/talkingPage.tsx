6803328a670260fd6bce0d15065c873e82a8e6e8
박시연 <xion2664@gmail.com>
2024-05-08 13:12:44 +0900

    [Feat] 3D asset, msg api, puzzle(ing)
    Former-commit-id: c57954e0c090aeddde9d32bf38c58a9a4aa990c6

--- bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7d742fd5b5bf5ca5f523552f9cc1889f44a5156e9b196aa3e8f7f7a45e53eac
-size 1639
+oid sha256:899264854e809c5223941ef81a5c83dac80c9a21dbcfc0b13538bace6a8cce06
+size 2164
--- bridgetalk-front/src/styles/child/talk/messagePage.style.ts
+++ bridgetalk-front/src/styles/child/talk/messagePage.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b19eabdef9839126b060bdc97a7d5d96a7f0bd6d25868566d25cbd69a5fe6e6
-size 1033
+oid sha256:f7602d08902b659bf9d3385611e58abfb2234a63385db38e6edf266e85ea34c5
+size 1090
