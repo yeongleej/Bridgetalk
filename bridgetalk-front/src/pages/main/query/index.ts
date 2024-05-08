efe99a3f81510f17c02c4c7de419c316241af361
조한빈 <whgksqls9999@naver.com>
2024-05-08 10:04:41 +0900

    [Feat] 프로필 추가 기능 구현
    Former-commit-id: 91e089a74a5ea32e390c44a9fe3a2232705a5d16

--- bridgetalk-front/src/pages/main/query/index.ts
+++ bridgetalk-front/src/pages/main/query/index.ts
@@ -2,3 +2,4 @@ export * from '@/pages/main/query/postSignin/postSignin';
 export * from '@/pages/main/query/postSignup/postSignup';
 export * from '@/pages/main/query/getProfileList/getProfileList';
 export * from '@/pages/main/query/postProfileLogin/postProfileLogin';
+export * from '@/pages/main/query/postAddProfile/postAddProfile';
--- bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88a02029d170ae4e7909847586b8574ddbce16f41afdd670778fcfe3d128e090
-size 6560
+oid sha256:e341beee8b7250d7fc53e7c7a4e783a33bdfe8711b41ee5e9f16d0c9efb1658c
+size 7084
