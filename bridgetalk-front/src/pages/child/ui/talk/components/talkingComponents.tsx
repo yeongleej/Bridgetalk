2a5452c83328a5ae8b4683649e1a22dfba8e1f36
조한빈 <whgksqls9999@naver.com>
2024-05-08 13:32:23 +0900

    [Feat] 아이 페이지 들어갔을 때 바로 대화, 녹음 시작
    Former-commit-id: 73569750d6b1b7a5eb1012e55387363c0ef3aaba

--- bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a27a80a52750a9c41872e62bc67338676d989ee5e844623ab1119fbfec9846b
-size 4452
+oid sha256:67debb36fba54f2aac44ae4d7b5d6aa3288d27616b37b6339dcd34b6b9dace5d
+size 5055
