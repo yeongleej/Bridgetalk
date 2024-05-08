2b67b860a6a71d2ffe84713453bd472fe41db61f
조한빈 <whgksqls9999@naver.com>
2024-05-08 15:35:17 +0900

    [Fix] 한 마디 전송하기 파일 webm -> mp3 형 변환 로직 추가
    Former-commit-id: 199fa3bcfb902b59a2916da1b84a1e2740624a78

--- bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
+++ bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
@@ -2,11 +2,11 @@ import { customAxios } from '@/shared';
 
 export async function getTalkStop(reportsId: number, setReply: any) {
   customAxios
-    .get(`/reports/talk-stop/${reportsId}`, {
+    .get(`/reports/talk-stop/${206}`, {
       responseType: 'blob',
     })
     .then((res) => {
-      setReply(res.data);
+      setReply(URL.createObjectURL(res.data));
     })
     .catch((err) => console.log(err));
 }
--- bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
+++ bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
@@ -1,16 +1,3 @@
-import { customAxios } from '@/shared';
-
-export async function postSendTalk(reportsId: number, audio: Blob, setReply: any) {
-  const formData = new FormData();
-
-  formData.append('reportsFile', audio);
-
-  return customAxios
-    .patch(`/reports/talk-send/${reportsId}`, formData, {
-      responseType: 'blob',
-    })
-    .then((res) => {
-      setReply(res.data);
-    })
-    .catch((err) => console.log(err));
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:50152c29e2e3a6e51ae6012a1204e143c8f7a15ce77380dc4089ce7b59aea3d7
+size 1395
