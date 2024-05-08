bd2efebf407847baf1f477c4f7cdade94f9e46d3
조한빈 <whgksqls9999@naver.com>
2024-05-08 16:03:55 +0900

    [Feat] Timer 공통 컴포넌트 분리
    Former-commit-id: 0cbfa32c3c1b368026b966b72b55e62d8c3908be

--- bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ccaa7228e3de1f838fe8b3fccf18a7f4b662b2b9c3da3a78aaccc064176b362
-size 1976
+oid sha256:857b49709707d35ad9e9c7bd81a88b48587503522c9732ffc451f40828176757
+size 2101
