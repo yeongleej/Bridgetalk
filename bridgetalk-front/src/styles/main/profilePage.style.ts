202fd8acb871d4bd21e671962c050ea94f0d8dde
조한빈 <whgksqls9999@naver.com>
2024-05-08 10:23:05 +0900

    [Feat] 프로필 삭제 모달 상태관리
    Former-commit-id: f416e1cb6c5c17067d7a72422c7ab37fdac89e5f

--- bridgetalk-front/src/styles/main/profilePage.style.ts
+++ bridgetalk-front/src/styles/main/profilePage.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13beccf9f3fd84747b29cf0878fd111ee1982728d017fdf081c08e2911bd3650
-size 2738
+oid sha256:03f686ad097c765b53ecf2abcdbe05da73a4e4b2daa765f6a9776a5429e6fd80
+size 2961
