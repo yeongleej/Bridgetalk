0dce845d6dc0dd14cde666e742d92bec5d9062e7
조한빈 <whgksqls9999@naver.com>
2024-05-08 10:37:04 +0900

    [Feat] 프로필 삭제 기능 구현
    Former-commit-id: c97279f497488622175e1a231203931bda3bc84e

--- bridgetalk-front/src/pages/main/query/index.ts
+++ bridgetalk-front/src/pages/main/query/index.ts
@@ -3,3 +3,4 @@ export * from '@/pages/main/query/postSignup/postSignup';
 export * from '@/pages/main/query/getProfileList/getProfileList';
 export * from '@/pages/main/query/postProfileLogin/postProfileLogin';
 export * from '@/pages/main/query/postAddProfile/postAddProfile';
+export * from '@/pages/main/query/deleteDeleteProfile/deleteDeleteProfile';
