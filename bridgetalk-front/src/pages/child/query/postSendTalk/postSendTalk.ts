1612ff0fc58dfa843279c57cb16a92e24e2909be
조한빈 <whgksqls9999@naver.com>
2024-05-08 17:13:43 +0900

    [Docs] FFmpeg 필요 파일 public 폴더 내에 저장
    Former-commit-id: f33e7d456fc4873d78b8ef99611de5f2a720ec73

--- bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
+++ bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb75b33cb708ddc0f5f832e2b07dd75bd8b48e029a8d0d6af84ed2c00b9d28a
-size 1196
+oid sha256:0a8d07d89442d4dc12dabab6cdd6a49c74e32549280c1d475fbf7b1171a6f5ce
+size 1414
