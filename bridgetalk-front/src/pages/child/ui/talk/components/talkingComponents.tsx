c4da904868b35cee493d64a11d58ba7436b7710c
조한빈 <whgksqls9999@naver.com>
2024-05-08 17:43:59 +0900

    [Refactor] 불필요 버튼 제거
    Former-commit-id: f07ed712d52089e303ce8b06f82135b8ca8a0f16

--- bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54470cfcbfc9e1bcc36592f70170bfcb34d5e0167318107b0008006ba31c9332
-size 4835
+oid sha256:4165ca334fc41ce7e7fccc77be3133300e51d4837e19e49f59441b708b83feee
+size 4843
