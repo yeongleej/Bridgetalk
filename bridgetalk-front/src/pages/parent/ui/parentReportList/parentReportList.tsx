d13201cb7967a4441da178ae8259de4efc9467e3
조한빈 <whgksqls9999@naver.com>
2024-05-08 11:14:02 +0900

    [Feat] value가 undefined인 리포트 리스트 응답값 예외처리
    Former-commit-id: 099233ba4fbd5657e54bcfcc116d995611e9cd40

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportList.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54692685c6254d77c482f0da10b0e6d918c0763f23ceabc272dec1c9dbd2c071
-size 2323
+oid sha256:2393804b8f2849621862abedf15c4cc8f2cf36e681f7acebe5b7216c4e8ee14b
+size 2429
