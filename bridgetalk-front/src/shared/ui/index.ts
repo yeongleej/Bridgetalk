522bb905e4fb3a053cc20b5b2bcbff35dd5ea0e6
조한빈 <whgksqls9999@naver.com>
2024-05-08 16:03:55 +0900

    [Feat] Timer 공통 컴포넌트 분리
    Former-commit-id: 6c52dc08caf16e9fd4ff36a70f6aa673bb0736de

--- bridgetalk-front/src/shared/ui/index.ts
+++ bridgetalk-front/src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export * from '@/shared/ui/examUi/examUi';
 export * from '@/shared/ui/modalSpace/modalSpace';
 export * from '@/shared/ui/backButton/backButton';
 export * from '@/shared/ui/homeButton/homeButton';
+export * from '@/shared/ui/timer/timer';
---
+++ bridgetalk-front/src/shared/ui/timer/timer.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:614b6190af938b6668d16e17a4970d17e9df750839abe93a0a5b8867cd6e1b0c
+size 642
