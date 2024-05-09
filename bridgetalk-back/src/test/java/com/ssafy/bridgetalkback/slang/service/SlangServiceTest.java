3120b21888c1be8ae5ee8b9f357fe0940abca68c
방소영 <bjo8244@naver.com>
2024-05-09 21:44:04 +0900

    [Temp] 실시간 stt 설정 및
    Former-commit-id: 0d645bc47bdb1d374b29bb0061f72ff498526585

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/slang/service/SlangServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/slang/service/SlangServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80b9094223c1bb9c3b5f9b1e80cfc68926dc72f7032cabd80534c2d6be343c4a
-size 2237
+oid sha256:10cf434169251635c8a77940ac9d3fb1d11991145db2af1f5159d6eb197b2ff7
+size 2287
