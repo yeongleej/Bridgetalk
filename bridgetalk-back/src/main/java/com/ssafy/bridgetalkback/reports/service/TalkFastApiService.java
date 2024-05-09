7d564f5e690fc1d36234dffb8dfb52f48e25d52a
이지영 <abcd23589534@gmail.com>
2024-05-09 16:05:44 +0900

    [Fix] TalkFastapi 호출 url 변경
    Former-commit-id: 793ef4e1f8f73658ff9bfdfb6f7e86a792576034

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkFastApiService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkFastApiService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a3b8294926fd66e337565ee4d1816602d522c3833f19c82fe416977ed75cd98
-size 1870
+oid sha256:1dbc136b897d40f8bb6891c4a2075a3a5b9654e5c464d7d1a1ebb3fa9e0b01dd
+size 1877
