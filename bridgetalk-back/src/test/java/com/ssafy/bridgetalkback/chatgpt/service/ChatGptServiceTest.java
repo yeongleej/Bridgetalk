d290c4a5f875dfb94f5dfefc4449f17c271157e1
hyun0-25 <lisa1072@skku.edu>
2024-05-09 11:19:26 +0900

    [Test] gpt->파파고 관련 테스트코드 수정
    Former-commit-id: 188aef6f86787fb5eb017a0032e7f0945f556047

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1acbcbc96275e69825e68c9867ad6058204a3e24ccec12c1fdb7637db578a4e
-size 3291
+oid sha256:ee892273b270a830640ee45e5d9e834ba4868f8d87c56e21b118b58140fdb426
+size 3399
