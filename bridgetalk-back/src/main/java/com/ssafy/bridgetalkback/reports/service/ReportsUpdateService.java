0a3c5cd4102074b867586b794b0ab2d661b0d9b6
hyun0-25 <lisa1072@skku.edu>
2024-05-09 11:19:01 +0900

    [Fix] 번역기능 gpt -> 파파고로 변경
    Former-commit-id: f8b77d1eac2ab27e29bdcb84785a5d04e24c5c52

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsUpdateService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/ReportsUpdateService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db430b1e0c18cfa0d89791b9c6669f3010d2e6b169725bf3c7cbea1f01e507c2
-size 6081
+oid sha256:91862511ae1eb3b353764cdd223de839999bba9f584e20679c227af1271bc50e
+size 6165
