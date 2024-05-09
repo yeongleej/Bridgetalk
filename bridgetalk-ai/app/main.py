041b465a41d0e090623946d411eefd0eb9e273f7
이지영 <abcd23589534@gmail.com>
2024-05-09 13:40:24 +0900

    [Feat] fastapi 로직 추가 & fastapi 호출 service 추가
    Former-commit-id: c7c18a792ae21c09c1aba98c0db2019b256c1165

---
+++ bridgetalk-ai/app/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84e721cc39d63d207cfaac2d504a6a7e0ba4b8266644b820a311b02e0f466e0d
+size 472
---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkFastApiService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a3b8294926fd66e337565ee4d1816602d522c3833f19c82fe416977ed75cd98
+size 1870
