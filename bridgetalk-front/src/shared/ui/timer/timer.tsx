d7416244913403e49a52c3c345c3045320c72e6f
조한빈 <whgksqls9999@naver.com>
2024-05-11 01:01:38 +0900

    [Design] 타이머 UI
    Former-commit-id: cac371a457d925e3ad188f05709e06164f7dfe3d

--- bridgetalk-front/src/shared/ui/timer/timer.tsx
+++ bridgetalk-front/src/shared/ui/timer/timer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f157378dba2553c3d5cc43fa4e0abce8d41aafa877ae051aeb7b9dbc31bad875
-size 1891
+oid sha256:9e14189a50726a7d9ff4b703a25a1ab36d07c9facdea64c6e2695183c1d4a4e2
+size 1985
---
+++ bridgetalk-front/src/styles/shared/timer.style.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:796010103c14478b168896763038c413e154ac23a4b0bc6dc87f37e773f839ce
+size 698
