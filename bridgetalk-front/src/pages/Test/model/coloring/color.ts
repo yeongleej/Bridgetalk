356d80304ae4bfeb38a3e6c3164e47b7344fa78f
조한빈 <whgksqls9999@naver.com>
2024-05-11 01:08:00 +0900

    [Feat] 토큰 저장 위치 변경
    Former-commit-id: eded01a2fb62c2bf27489128620feafb8bdf7fea

--- bridgetalk-front/src/pages/Test/model/coloring/color.ts
+++ bridgetalk-front/src/pages/Test/model/coloring/color.ts
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0e2423442b5767bbe2672a589edb91b4a6e58e1b6f4f80ccddc7c06c2b5843e2
-size 153
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+export type CanvasContext = CanvasRenderingContext2D | null | undefined;
