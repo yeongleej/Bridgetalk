e36cac3c12cde33f2826deda52cc583c27869d3d
sunkyoung <yskkjdh@gmail.com>
2024-05-10 15:14:14 +0900

    [Test] 육아정보 크롤링 API 관련 Service, Controller 테스트코드 작성
    Former-commit-id: 5833dd9c1a586551657a4e3d7ec49f88a90aa2fa

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/common/ServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c48279559f0df64d2736747bf7cf6ca8a1b22d8faec8da74f3b773c8b6c9a463
-size 1375
+oid sha256:15d5007ddf2be653758174671a263c1311e788bc597c1a6dc30dd28d445f7abc
+size 1682
