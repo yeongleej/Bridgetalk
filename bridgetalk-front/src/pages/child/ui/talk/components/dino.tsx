ab252ecf5cc5ea24c27a50dc5d65dffb5df6a485
조한빈 <whgksqls9999@naver.com>
2024-05-11 00:19:45 +0900

    [Feat] 아이 대화 관련 State 정리 및 대화 종료시 타이머 정지
    Former-commit-id: fcc96f4c2b2ff220000a41fcd275a4586d5149d9

--- bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d5f39ac7978fdaf84d06fd353ce628249981d6a032a7f0d2d495e7c5f4c5826
-size 965
+oid sha256:368e8ee621ff248498b8185bfb02b7f95d0c731ade139f2e4a6c4f2ba1ab4cb6
+size 1178
