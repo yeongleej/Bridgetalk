08c0bb83a67a4d41ef650b5fbc7bcf5d20febd5a
조한빈 <whgksqls9999@naver.com>
2024-05-10 13:39:51 +0900

    [Feat] 부모 녹음 관련 언어 설정
    Former-commit-id: 6db62a71d1ead5ebee783b485b532479ff9d8846

--- bridgetalk-front/src/shared/ui/homeButton/homeButton.tsx
+++ bridgetalk-front/src/shared/ui/homeButton/homeButton.tsx
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3d77e1f8e0d0b803383f011001ccf24ce6fcc29463a06e3f1b3c55c2c2d0fe4b
-size 397
+import { NavigateFunction } from 'react-router-dom';
+import * as S from '@/styles/shared/homeButton.style';
+interface Props {
+  navigate: NavigateFunction;
+}
+
+export function HomeButton({ navigate }: Props) {
+  return (
+    <S.Wrapper
+      className="back"
+      onClick={() => {
+        navigate('../start');
+      }}
+    >
+      <img src={'/assets/img/homeicon.svg'} />
+    </S.Wrapper>
+  );
+}
