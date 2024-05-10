9838b5d8436ae1a009d4d13a504ad1275c42ec8c
조한빈 <whgksqls9999@naver.com>
2024-05-10 09:25:07 +0900

    [Feat] 슬랭 페이지네이션 적용
    Former-commit-id: b70d5e6784d0cdd900a2154ee74e9d24e0c22ac3

--- bridgetalk-front/src/pages/parent/ui/parentInformationWord/parentInformationWord.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationWord/parentInformationWord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21193b83634fd04d3e2ab7686cedfa7b536d2fac05da5a83a78a71419450cfb5
-size 1466
+oid sha256:431d02060b9ca2ae21fe345c5e980c0a8a5a5bab51d114809ece11b5fb7f47a9
+size 3403
