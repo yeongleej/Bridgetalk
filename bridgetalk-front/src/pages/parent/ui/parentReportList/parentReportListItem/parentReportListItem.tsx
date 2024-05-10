cc817caf963d5447087a5952fda91123521c16f9
조한빈 <whgksqls9999@naver.com>
2024-05-10 11:12:39 +0900

    [Feat] 리포트 리스트 언어별 조건부 렌더링
    Former-commit-id: 9e4229416bb23e2a0fca9fb900a887d0c0b14f9f

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0eca8b9b4fabf9dc0d357f198e1e322309e0aeb079e162224d8861c7c625e63
-size 1166
+oid sha256:3679913bb55956ebfbbb8d00e82cc94e990cf08d1c555de793914395f5d27c63
+size 1749
