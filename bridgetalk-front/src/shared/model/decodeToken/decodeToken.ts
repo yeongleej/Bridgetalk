9384280ebb1589721b0982573d2d34c77d01ad20
조한빈 <whgksqls9999@naver.com>
2024-05-11 01:08:43 +0900

    [Refactor] 토큰 디코드 함수 로직 변경
    Former-commit-id: 676eec3a37cfaa0ca04ac6d7438d01ba3a814f2a

--- bridgetalk-front/src/shared/model/decodeToken/decodeToken.ts
+++ bridgetalk-front/src/shared/model/decodeToken/decodeToken.ts
@@ -1,9 +1,9 @@
 export function decodeToken(tokenType: 'access' | 'refresh') {
-  if (sessionStorage.getItem(btoa(`${tokenType}` + process.env.REACT_APP_SECURE_CODE)) === null) {
+  if (localStorage.getItem(btoa(`${tokenType}` + process.env.REACT_APP_SECURE_CODE)) === null) {
     return null;
   }
 
-  return atob(sessionStorage.getItem(btoa(`${tokenType}` + process.env.REACT_APP_SECURE_CODE))!).split(
+  return atob(localStorage.getItem(btoa(`${tokenType}` + process.env.REACT_APP_SECURE_CODE))!).split(
     `${process.env.REACT_APP_SECURE_CODE}`,
   )[0];
 }
