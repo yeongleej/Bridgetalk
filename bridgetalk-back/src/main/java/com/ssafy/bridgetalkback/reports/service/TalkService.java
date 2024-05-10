490fe05b3e7bf24a6df8bcb574f56065d2c1c11a
방소영 <bjo8244@naver.com>
2024-05-10 14:45:37 +0900

    [Refactor] 대화 시작 생성 및 업데이트
    1. Redis에 임시 저장
    
    
    Former-commit-id: bfe8095b024973b6fcb1e80aa72c598aaf89d88f

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05d516c7056f3e5d3a2e40748525e189a330ea09952ac954b7c6516ce078834d
-size 4537
+oid sha256:b5bbd87e34acb408235b4e7ae888bc837bd6c53b2887f45f280dd216b28f33cf
+size 5325
