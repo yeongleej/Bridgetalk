cfd73ae852ca3fdc3f7a3e3fc576c5fc5938de75
조한빈 <whgksqls9999@naver.com>
2024-05-11 00:22:06 +0900

    [Feat] 대화 종료 후 다시 다이노 눌러서 재시작 못하게 하기
    Former-commit-id: e9d3d20ef8b208b8442ddcb0488f517c3c9a41f6

--- bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:368e8ee621ff248498b8185bfb02b7f95d0c731ade139f2e4a6c4f2ba1ab4cb6
-size 1178
+oid sha256:fe75d5b323c40da6292b2910216c9b51828a7340ec5fd94d7dd3c77ef756ad2b
+size 1310
