f937d36133d2e5022da3a7cd2b714e3392ba7ca1
조한빈 <whgksqls9999@naver.com>
2024-05-10 12:48:33 +0900

    [Fix] ffmpeg 설정 파일 배포시 저장되게 설정
    Former-commit-id: 6cc8de24ee368eea728f9f025421f9a576b01129

--- bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
+++ bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1283587bee8bae8252da1803f991cfcaf020e29fefc18c49d39413d560f0cc45
-size 322
+import { customAxios } from '@/shared';
+
+export async function getTalkStop(reportsId: number, setReply: any) {
+  customAxios
+    .get(`/reports/talk-stop/${reportsId}`, {
+      responseType: 'blob',
+    })
+    .then((res) => {
+      setReply(URL.createObjectURL(res.data));
+    })
+    .catch((err) => console.log(err));
+}
