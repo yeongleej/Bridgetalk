adf0e4c11ee3c6f21266a15ce6c86fb077ceaf6c
방소영 <bjo8244@naver.com>
2024-05-10 13:28:05 +0900

    [Refactor] 대화 내용 Redis 적용
    1. 실시간 대화 Redis에 update
    2. 정상 종료시 Redis의 내용을 reports 생성 후 저장 하고 삭제
    
    
    Former-commit-id: d044354513d115f1f63e89e70d103baec0d887eb

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ecd85544bcd2695b9e610cf17221b49a8140ec8b1c15ad1f91984e4bca83882
-size 4071
+oid sha256:05d516c7056f3e5d3a2e40748525e189a330ea09952ac954b7c6516ce078834d
+size 4537
