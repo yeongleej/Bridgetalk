24d73043184e0d4df9e5aabd685e1c6171b019a8
조한빈 <whgksqls9999@naver.com>
2024-05-11 00:26:18 +0900

    [Feat] 다이노 눌러서 시작하기 전에 타이머 정지
    Former-commit-id: 46d86467cd08702beb64bcc7a6a8a34d91fad88a

--- bridgetalk-front/src/shared/ui/timer/timer.tsx
+++ bridgetalk-front/src/shared/ui/timer/timer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ded819e828d4d5b740f03d3e7376f8fd51259b35ce8ae9d2d07d59ecc809dc8b
-size 1832
+oid sha256:f157378dba2553c3d5cc43fa4e0abce8d41aafa877ae051aeb7b9dbc31bad875
+size 1891
