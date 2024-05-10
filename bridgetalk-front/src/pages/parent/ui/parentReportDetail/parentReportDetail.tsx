3841f72c3a37fc2befec04732f996438777d778b
조한빈 <whgksqls9999@naver.com>
2024-05-10 14:25:53 +0900

    [Design] 솔루션 스크롤 적용
    Former-commit-id: 07f00aed46899eab6f9f8e16267cbbc2aea3b559

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adeab05f9eb687715e07b42ef87a685d4f4486188f922a529b349f6d64d86f3a
-size 3405
+oid sha256:129432dd74ce2e1d64602a01e8ad9ff6226211b0499948af2ddd630ae5cf6786
+size 3470
