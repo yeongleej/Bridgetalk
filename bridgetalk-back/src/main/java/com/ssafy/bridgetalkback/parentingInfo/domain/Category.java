86157bc67df69fab19c8ef27a26e2ed2e74fd019
sunkyoung <yskkjdh@gmail.com>
2024-05-11 23:32:33 +0900

    [Feat] 없는 카테고리로 조회 요청 시 예외 처리 작성
    Former-commit-id: 6d860d6252bf55b8b941b7788a5a9720029f3fb6

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/Category.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/Category.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29b47f288fef7c993ade2efa9b2b3b91234eae0dce126ed6e56c802cf78cfd70
-size 740
+oid sha256:a8a60ae6ecc1025bf861fb834b324f922997b0dbe29d34360f315aaa457086af
+size 1223
