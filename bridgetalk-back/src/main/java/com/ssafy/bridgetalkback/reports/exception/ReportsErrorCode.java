3162f2ea056f96cb50a1a3b38ca05ed64c0c3596
hyun0-25 <lisa1072@skku.edu>
2024-05-11 18:14:19 +0900

    [Fix] 오타 수정
    Former-commit-id: 671c2a0b7a0f37b21430f9dc0d3813bbdcea0c5e

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/exception/ReportsErrorCode.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/exception/ReportsErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d13ccbe2e26bf78ebcb6d6894287abaf3197d95ced82a44f36618dd7ec96b08
+oid sha256:7fcc200513c3527dbe6abef0da28af19f996a9e3b7b5d19bc50c2227939d7093
 size 1145
