91679be433e9cc61a905f883d49f0d0047efaaa3
이지영 <abcd23589534@gmail.com>
2024-05-11 16:21:36 +0900

    [Feat] 대화 종료 로직 추가 & test code
    Former-commit-id: 3e2f683a58a20d9354298ea558546cddcdd9b7d2

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4770a756246213cbd41c37ba037ac3f1b670fa5a9c2b318f9a5e6ba5251bf3b2
-size 5555
+oid sha256:428c65e80bab9d0b933880b4c1aad37a3b574fc8ba1da0b975cf7097c5855716
+size 5999
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/service/TalkService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:536decaf2a58a634b579cbbd0e995c0c33d147ee0fd35318657f9870fd00d349
-size 7531
+oid sha256:569be184548f1c29d6da6f3dffd160e5cfb9280a7fe51ca09ccd7b96978c400a
+size 8197
