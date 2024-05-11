fdd8bb14293afda173c81c8590388ba139f8975b
sunkyoung <yskkjdh@gmail.com>
2024-05-12 00:51:21 +0900

    [Refactor] 조회 관련 메서드명 get 붙인 걸로 네이밍 수정
    Former-commit-id: 7f1b23fbe9e415ac2018cb1c8379ea914fea18cf

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/controller/ParentingInfoController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/controller/ParentingInfoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff3de376526dd4084135cf0280cee9c8ff8ba8fb4df09407584098c99186e9d8
-size 2243
+oid sha256:397a17dd6a0f506801bd1508f08f7936a731dd80b595fadc7eb7c8445732971d
+size 2255
