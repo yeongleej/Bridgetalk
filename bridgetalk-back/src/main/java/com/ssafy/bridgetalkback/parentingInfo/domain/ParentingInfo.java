8fdcc82fe07220ee139365b567b391a69f257522
sunkyoung <yskkjdh@gmail.com>
2024-05-11 23:27:56 +0900

    [Refactor] Age -> Category로 Column 명칭 변경에 따른 추가 수정
    Former-commit-id: 5e6ee044aecd5625d6ecf1b2b6bc96cb46a77641

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/ParentingInfo.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/ParentingInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6df327b2c9f6294b33666aa13ca6150fe274f7f466a56acdf1bf90c8e8b19d5
-size 1911
+oid sha256:ac46ad0f039bfe1aefd44514db7589898a18c0db5329a20cf27301bb2e751b82
+size 1598
