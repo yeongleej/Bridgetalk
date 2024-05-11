6a466967c36d89457a1c9f5a4464dfd02a5a29aa
sunkyoung <yskkjdh@gmail.com>
2024-05-12 01:03:41 +0900

    [Refactor] 불필요한 import문 제거
    Former-commit-id: a0ef8652a6c2780ed4bb4b9d753bff7ded21b1b5

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a867cc648b6b0d08cc2894ef02e4bf90e6508829de8190aa41adb41890daaa47
-size 2037
+oid sha256:570298f3f082be6fbb63d7b0e7821fad665395d007af279e4553494177e4e26e
+size 1942
