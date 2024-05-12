a33f955241b11e18324396bcebc10239b20941e5
sunkyoung <yskkjdh@gmail.com>
2024-05-13 05:59:47 +0900

    [Test] 테스트코드 메서드명 수정 및 throwExceptionByUserIsNotParents() 메서드 작성
    Former-commit-id: 6a83e07a8b2044978be7dfab6723e402afa7f342

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/controller/BoardsControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/controller/BoardsControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d2149c6f5d5ea797a095527f7f8b3720a9a6285dbbbc4b885dd9f7f368f8cec
+oid sha256:a281c26ebf8e949312aae58e01ec775e186175d897452bd27afd050f11a7a762
 size 5590
--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80722ef0ba44564405fb3833e4dae1d575df2523c2fdb161f74d77ed31beb76d
-size 5188
+oid sha256:8b76dc04da42e1df0d11b27fabfd330be14a4c2b1707bb4746c7c00e7a2e6265
+size 4621
