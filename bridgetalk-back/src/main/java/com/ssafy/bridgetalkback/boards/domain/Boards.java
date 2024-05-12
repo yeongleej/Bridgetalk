c85c04352b9d669913dbd10238014eb3c1f61d61
sunkyoung <yskkjdh@gmail.com>
2024-05-12 23:43:20 +0900

    [Feat] Boards에 총 좋아요수를 저장할 likes 컬럼 추가
    Former-commit-id: f47dc836d4b62eb1a6e7a824df10b29459c2a6a5

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/domain/Boards.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/domain/Boards.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c08c25bf8530fcd88be3ed216590a769513e474ee8fd25d9e5b5d46779b491b3
-size 1638
+oid sha256:c32aa41aa565d9d73b47558f60eec0013661f552d466b9b654ae1e97f834494a
+size 1715
