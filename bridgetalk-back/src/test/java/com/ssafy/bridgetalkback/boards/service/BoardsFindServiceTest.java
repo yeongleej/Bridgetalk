58875aa7c4cef41d90b6cc7857f8d528fcdf7d07
sunkyoung <yskkjdh@gmail.com>
2024-05-13 05:25:57 +0900

    [Test] (커뮤니티) 게시글 상세조회 API 관련 Service, Controller 테스트코드 작성
    Former-commit-id: ceea35ecb16210a3ec719286bb172add46bbc10a

---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsFindServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c76cfa45f6e7ef8285fea48c7c1ba52ddcb8f4219765e6d1379ec24da246046
+size 2220
---
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80722ef0ba44564405fb3833e4dae1d575df2523c2fdb161f74d77ed31beb76d
+size 5188
