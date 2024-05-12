9633c5cf9f1181ffcc2ea9318baa2d582010fd2d
xion <xion2664@gmail.com>
2024-05-13 08:39:25 +0900

    [Feat] loading pages but wrong branch
    Former-commit-id: 38f2a388b8c90676d26b4ab2a46953102e923075

---
+++ bridgetalk-front/src/pages/child/ui/game/puzzle/stagePage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1da8055f542482bca02c3f5bc0ab0f4ac455c615b3de022f0d5358bebe9fa2d
+size 1148
---
+++ bridgetalk-front/src/shared/ui/loading/loadingScreen.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1b4547605f84872e8dc8bc5cb75afba6cd8ede25413bfc113a372f2c9bff8cf
+size 393
