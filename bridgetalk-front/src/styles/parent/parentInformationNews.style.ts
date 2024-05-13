c835b74a974e7ce0dc0d9e5b2e169d82fbeb891e
조한빈 <whgksqls9999@naver.com>
2024-05-13 11:03:08 +0900

    [Design] 육아정보 리스트 레이아웃
    Former-commit-id: 5be90610612f86f0201e5c6000e0389e4f0c11e7

--- bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fb71fb76d8881454f9b86f2cbcfd7137846acae7d637ce8ccf52b56d4c02064
-size 1582
+oid sha256:bc0826bac8c21c49dfc0e0c22150cd21a2be7ba596c50fb1acd4510f1d73fa14
+size 1889
