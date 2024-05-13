84d444d024af7e90966e89fcc954dace104dff2d
방소영 <bjo8244@naver.com>
2024-05-13 15:27:07 +0900

    [Feat] 편지에 대한 키워드 추출 api 구현
    1. 추출된 키워드를 바탕으로 서버 db에 있는 keyword로 매핑
    
    
    Former-commit-id: d8fb52d25770668f33dce6a60ef44a275c39a1de

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b6c9d16a0634275b8c0f0adf9c2b8acc73a189183355714a140ab8ce9c9b663
-size 6387
+oid sha256:01cdd33ae87b8e4098d5677a09b37fc4ee3b3aedd6bf5c50f1a61c260a4591e8
+size 7785
