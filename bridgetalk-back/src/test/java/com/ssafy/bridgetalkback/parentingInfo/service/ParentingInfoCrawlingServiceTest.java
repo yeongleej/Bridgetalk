20943530ee2de3b3d31819e64e0345703bf61327
sunkyoung <yskkjdh@gmail.com>
2024-05-13 15:15:10 +0900

    [Refactor] 크롤링 서버 접속 오류에 영향 받는 테스트코드 제거
    Former-commit-id: c696f02e7d9a25e54424b01db3a782b469d506a5

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoCrawlingServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoCrawlingServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be3de6055c0b6076561d241a4351da6adcbe5e31ec54b6460c41abc47569f78d
-size 2711
+oid sha256:4783f9858eb89e1a63db7095358673f6b60c02ae720e46d0f885d64cf48b4008
+size 1718
