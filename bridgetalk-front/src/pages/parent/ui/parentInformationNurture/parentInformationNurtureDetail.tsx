3384cd31332e0f99c3ffe894db3e30cf8a836d7c
조한빈 <whgksqls9999@naver.com>
2024-05-13 11:06:59 +0900

    [Design] 육아정보 뒤로가기 버튼
    Former-commit-id: 30d4c44ca65a3432587365c29e26397cbe0707a6

--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurtureDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurtureDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a73a2a10a7268cee08a07260ccc1f2f1fa0cdfbe7fec04241c83bbbfbc7ffe00
-size 784
+oid sha256:887fc3d59d35862a8ffcf5914de5f8959e31e070905b2a6540b996aa07c245b6
+size 973
