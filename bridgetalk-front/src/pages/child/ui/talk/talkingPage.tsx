01c90499036ac80fb9e5d54cd673595315092472
조한빈 <whgksqls9999@naver.com>
2024-05-14 00:33:04 +0900

    [Feat] 타이머 4:40에서 대화 종료
    Former-commit-id: 70eb8f7c49613e84bda654fda4d4466dc16de54c

--- bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3db5a405c7f0de415b256462dcc1b06def0d9a67da80bf659bb56c3e82033b1
-size 3867
+oid sha256:a61571885c89675fb61e7349c513f1749aa0653661dca3b17c6d309857f431bd
+size 3993
