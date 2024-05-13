16d43688db25c28bf4135f46f2fc99601c86e45e
조한빈 <whgksqls9999@naver.com>
2024-05-13 23:06:14 +0900

    [Feat] 대화 종료시 talk state 초기화
    Former-commit-id: e5b82c7455560b1c590af0f7fd98d397c6a1319d

--- bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fe49376ce93a563a5a392a5d333939a46121cdc81609c9595779f39d9a50ba9
-size 6951
+oid sha256:c80ce43a0326d578119e91ed8e54d4a1d92405e7952e1e2f8725ac25d0a0cf8f
+size 6987
