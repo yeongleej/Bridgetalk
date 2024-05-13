3c6100296773b99a7b05850e5a5e2e792a97cbd0
조한빈 <whgksqls9999@naver.com>
2024-05-13 13:50:53 +0900

    [Design] 아이 페이지 CSS
    Former-commit-id: ea4a7093c048c3156be6d7edfcf0af9b90a8b259

--- bridgetalk-front/src/styles/child/childMain.style.ts
+++ bridgetalk-front/src/styles/child/childMain.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f385c753013459be5d3f0c3aa7db3c3ce50abb5b44f6c8fc76b02de1aebd6b51
-size 2222
+oid sha256:acf1eb53469716ca1c6c5623ebb7bf42e8f7f80187d5b221ef4b3b2dd4d4782a
+size 2312
