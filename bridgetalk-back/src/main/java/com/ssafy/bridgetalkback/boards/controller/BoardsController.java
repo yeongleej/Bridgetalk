0115e45109d84adb6ceee6a6e08680e236bd1498
sunkyoung <yskkjdh@gmail.com>
2024-05-14 01:38:29 +0900

    [Feat] (커뮤니티) 게시글 리스트 조회 API 구현
    Former-commit-id: 571e89b01d72b13e6293150de9f7bb46945e542b

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba997b4b7a5cee14ee3d3ecd6333613fd93fd23e5f1206cd4be2fa9624ab78cc
-size 1153
+oid sha256:0eb61badb05ba1e838d83a5e8a960a590245e2df0a6bf815664c60d1014d3c68
+size 2669
