40ee353610869ff97c7540cd3e674f0967ae0bdb
조한빈 <whgksqls9999@naver.com>
2024-05-13 10:24:42 +0900

    [Design] 카테고리 변경시 페이지 0으로 초기화
    Former-commit-id: fc0379edb0a738109cc3b097dabdade491ac9bfa

--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46d5885e849a0b667a7e469e2f1658d0295de6cd4e5ed4c78fbca1c0fe7ca1d5
-size 2817
+oid sha256:ada3e590c24dcad13c95a09ca8e33de5f3f0310b01d9d42ed4b853f8e6806385
+size 2944
