c8773fa95fcb87a09f04603b505a5c6d94e622aa
조한빈 <whgksqls9999@naver.com>
2024-05-13 13:48:12 +0900

    [Feat] 아이 페이지 홈버튼, 마우스 호버 CSS
    Former-commit-id: 3f4edc7d3a72422a6e13e29b89d51a904b7445e5

--- bridgetalk-front/src/shared/ui/index.ts
+++ bridgetalk-front/src/shared/ui/index.ts
@@ -2,4 +2,5 @@ export * from '@/shared/ui/examUi/examUi';
 export * from '@/shared/ui/modalSpace/modalSpace';
 export * from '@/shared/ui/backButton/backButton';
 export * from '@/shared/ui/homeButton/homeButton';
+export * from '@/shared/ui/homeButton2/homeButton2';
 export * from '@/shared/ui/timer/timer';
