d86f8aedf20302f9c1fb31db505546b85e1a9260
sunkyoung <yskkjdh@gmail.com>
2024-05-13 15:16:00 +0900

    [Refactor] 불필요한 조인 제거를 위해 @ElementCollection 제거, StringConverter 구현 후 적용
    Former-commit-id: 3a374d9419a94280872f00d46f73342a7dc6bbf4

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/domain/Reports.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc5abc729040948233dcd5ae49f72e372298fe90f7aa9b1f91be450bad621f47
-size 2875
+oid sha256:9c05afbb728ae1ad2a59a89c497f21d5572baa0046d8228d77f82012722a9f49
+size 2949
