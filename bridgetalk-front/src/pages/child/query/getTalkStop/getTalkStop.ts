e5adfee4fc7b38d08a71b204aa3f23169d07a0ff
조한빈 <whgksqls9999@naver.com>
2024-05-13 23:20:36 +0900

    [Feat] 대화 종료시 매화 마치기 버튼 다시 누르기 방지
    Former-commit-id: aa809bc16d262db70e1dffee59f52e663a29aca0

--- bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
+++ bridgetalk-front/src/pages/child/query/getTalkStop/getTalkStop.ts
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6699582be356eeb1440f41afc2ffff3fb5548f950a5efd816a639313fcdfc887
-size 742
+import { customAxios } from '@/shared';
+
+export async function getTalkStop(setReply: any) {
+  customAxios
+    .get(`/reports/talk-stop`, {
+      responseType: 'blob',
+    })
+    .then((res) => {
+      setReply(URL.createObjectURL(res.data));
+    })
+    .catch((err) => console.log(err));
+}
--- bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/talkingComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c80ce43a0326d578119e91ed8e54d4a1d92405e7952e1e2f8725ac25d0a0cf8f
-size 6987
+oid sha256:df996d4ad25fb703beea5e6afeaff479a8b9edd0046eed84777e6d6b47991b3c
+size 7128
