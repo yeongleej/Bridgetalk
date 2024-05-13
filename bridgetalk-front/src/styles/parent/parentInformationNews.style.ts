e14f8de2ffbc814f3e83b3cebceca0b4e6786df8
조한빈 <whgksqls9999@naver.com>
2024-05-13 11:18:10 +0900

    [Fix] 언어 변경 시 리스트 리렌더링
    Former-commit-id: d729eeb4d4bfeb33f502516c99b3cd939d94d643

--- bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc0826bac8c21c49dfc0e0c22150cd21a2be7ba596c50fb1acd4510f1d73fa14
-size 1889
+oid sha256:d06e8f5d206bdfcc5ffd5c1a58c7dd47e5a8d0830902eb8c329588f80fe43e9c
+size 2347
