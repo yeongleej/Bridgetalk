73e613ce2a11f32f58821ee4ecde817cab491a41
조한빈 <whgksqls9999@naver.com>
2024-05-13 10:23:32 +0900

    [Design] 육아정보 페이지 디자인
    Former-commit-id: ade59bf2d4abb11e0a1a47632d3276076ce74b10

--- bridgetalk-front/src/pages/parent/model/handleNurtureInfoList/handleNurtureInfoList.ts
+++ bridgetalk-front/src/pages/parent/model/handleNurtureInfoList/handleNurtureInfoList.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e93d73a72c75b9c463fccaac41410b16250caacc73c9308396db1f8ed8b3dd6a
-size 568
+oid sha256:95129398e3a5062cee65986da85463dea1c13f0135de29d691c884614f587edc
+size 573
--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c39114a201b89e1e7b3373e9e035c8588bba07b0f6b5121820a931fa0a48b97c
-size 2049
+oid sha256:46d5885e849a0b667a7e469e2f1658d0295de6cd4e5ed4c78fbca1c0fe7ca1d5
+size 2817
