d738d47a7f2fa478719346f3d7d9aa4400e05971
조한빈 <whgksqls9999@naver.com>
2024-05-13 14:21:21 +0900

    [Feat] 마이크 권한 에러 캐치
    Former-commit-id: 7ad39046fac2e9b5dbbf7f8fb65584664a34431a

--- bridgetalk-front/src/shared/model/errorCatch/errorCatch.ts
+++ bridgetalk-front/src/shared/model/errorCatch/errorCatch.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1c8835b4c873417df80c06c98a534cccd0000abf39032a435239692a85b428d
-size 1093
+oid sha256:0d3af61b27eb634b814a08de7e4f722aceb669257c979c0daa6d00f8c7ec8ab8
+size 1275
