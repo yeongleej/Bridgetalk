5e50334500a4e8d20395dadb226bdfea68959e24
조한빈 <whgksqls9999@naver.com>
2024-05-13 10:05:48 +0900

    [Feat] 육아정보 페이지네이션
    Former-commit-id: fa76518d1a1650c9cb655cd6a339b49586d74027

--- bridgetalk-front/src/pages/parent/model/handleNurtureInfoList/handleNurtureInfoList.ts
+++ bridgetalk-front/src/pages/parent/model/handleNurtureInfoList/handleNurtureInfoList.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ddcaefde021491f9dd7f045d7a5a9e83d27f66c5c18a4aa8e9bdfe3ccf48ca
-size 300
+oid sha256:e93d73a72c75b9c463fccaac41410b16250caacc73c9308396db1f8ed8b3dd6a
+size 568
--- bridgetalk-front/src/pages/parent/query/getNurtureInfoList/getNurtureInfoList.ts
+++ bridgetalk-front/src/pages/parent/query/getNurtureInfoList/getNurtureInfoList.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:857551fe44cd1a114e944245df71a497e3ad1ea3054344f47096a9c25d4cc317
-size 188
+oid sha256:bf0417db7134f1a33d55e5172230fe7fcf19b76066c3d36eb0fa7cfd78964bed
+size 339
