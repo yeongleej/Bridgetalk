c137d85e2112474321279f1a7da024b2a03753a2
조한빈 <whgksqls9999@naver.com>
2024-05-13 13:38:25 +0900

    [Feat] 아이 대화하기 뒤로가기
    Former-commit-id: f301e7808fa8ad1e0745204941ec14ab64a390cc

--- bridgetalk-front/src/pages/child/model/handleTalkEnd/handleTalkEnd.ts
+++ bridgetalk-front/src/pages/child/model/handleTalkEnd/handleTalkEnd.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:708d1f202f74843f1acf811f98423989a9073095021b86f7a87aa53869463feb
-size 564
+oid sha256:54794386fdf07b2c8b480fec1def09dc14ccb74b6fdcac3d07d3d6e4d72d4102
+size 681
