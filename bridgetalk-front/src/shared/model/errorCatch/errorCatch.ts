d77b46ea20881bf561ca7c76685f369c530cf6c6
조한빈 <whgksqls9999@naver.com>
2024-05-13 13:54:04 +0900

    [Feat] GPT_004 에러 캐치
    Former-commit-id: cb5ace05cbe7cbf7ed6c07198511533f36bbcd2b

--- bridgetalk-front/src/shared/model/errorCatch/errorCatch.ts
+++ bridgetalk-front/src/shared/model/errorCatch/errorCatch.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:610b28418b4483b1af5f3f35870e6064e8ce1f5766a3c9e6d05b0581de3d6700
-size 969
+oid sha256:a1c8835b4c873417df80c06c98a534cccd0000abf39032a435239692a85b428d
+size 1093
