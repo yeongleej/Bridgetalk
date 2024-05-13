bfc1ebdda23334e61888a9a9209ece8080f05ee2
방소영 <bjo8244@naver.com>
2024-05-13 15:27:07 +0900

    [Feat] 편지에 대한 키워드 추출 api 구현
    1. 추출된 키워드를 바탕으로 서버 db에 있는 keyword로 매핑
    
    
    Former-commit-id: 54081fa21fc1a32e934ca4afdb27558a8e7088e7

--- bridgetalk-front/src/pages/parent/ui/parentInformationNews/parentInformationNewsListItem/parentInformationNewsListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNews/parentInformationNewsListItem/parentInformationNewsListItem.tsx
@@ -1,18 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-
-interface Props {
-  newsId: number;
-  newsTitle: string;
-  newsDescription: string;
-}
-
-export function ParentInformationNewsListItem({ newsId, newsTitle, newsDescription }: Props) {
-  const navigate = useNavigate();
-
-  return (
-    <button onClick={() => navigate(`${newsId}`)}>
-      <div>{newsTitle}</div>
-      <div>{newsDescription}</div>
-    </button>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3fff977cd8d9e3622f9c662a4b3a50239cc2859a26c02a6c4065c13f25f52372
+size 413
--- bridgetalk-front/src/styles/parent/parentInformationMain.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationMain.style.ts
@@ -1,15 +1,3 @@
-import styled from 'styled-components';
-import { buttonNormal } from './common.style';
-
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 10svw;
-
-  button {
-    img {
-      width: 100%;
-    }
-    ${buttonNormal}
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:0daed4a893bec577bb0696f97d6a6a3e6354dcb79a7231e642bf3d15d6f34cca
+size 257
