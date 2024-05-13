ce51f107d800292e709bdd722e490e2d08b5fb8b
조한빈 <whgksqls9999@naver.com>
2024-05-13 13:38:25 +0900

    [Feat] 아이 대화하기 뒤로가기
    Former-commit-id: ea123abfdcddd2de6678381ba0d016962ea53ff5

--- bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/talkingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9a21e10ac5533bfa587a0bf2f3185ec4c3e6f4edceb43d91012736e69ff2f3d
-size 4114
+oid sha256:0b633860e83721769e1e73e85483ed9be31b41f1857ae47f51b8bc17a67e63f4
+size 4124
