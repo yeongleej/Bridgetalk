95d18634b08cef41fe4eea991527ae50a3726b22
방소영 <bjo8244@naver.com>
2024-05-14 09:55:42 +0900

    [Refactor] 키워드 매핑 되지 않을시 사랑 이미지 반환
    1. 키워드 매핑 오류 발생시 사랑 이미지 반환으로 변경
    
    
    Former-commit-id: 342664c792e7d5007b0f70a644d49fdfd6bdac7d

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a44d5ea9a0b7f201635fc7bd1cb90226b9e15e62d84506ae78aa095dbf08a8d6
-size 10918
+oid sha256:82919c1439d8f740c7cd000cb8fb770d6c372ed815684fa156f86351411affdd
+size 11084
