b4f4e56606d10c287a6c94c3310a13030ca42c04
hyun0-25 <lisa1072@skku.edu>
2024-05-14 09:20:28 +0900

    [Refactor] 게시글 번역 과정에서 영어 추가
    Former-commit-id: e23a0d5bb16c2905120e98a97c23de8373cd43f4

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/domain/Boards.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/domain/Boards.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:359718ecbeea54ebac313f899ec23a7efe7442cfd7ee5bad319ffd09d2310a80
-size 2418
+oid sha256:aa1dc3c6d944d2ecb3027814f2cc3899c5d490d920dad3e1882369b6b216f002
+size 2419
