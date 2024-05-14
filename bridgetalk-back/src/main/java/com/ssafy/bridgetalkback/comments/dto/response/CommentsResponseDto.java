59dfea1e7daf3e4620cc05f3304307497d5354cc
hyun0-25 <lisa1072@skku.edu>
2024-05-14 09:20:58 +0900

    [Refactor] 답변 번역과정에서 영어 추가
    Former-commit-id: 9df1c3a2f7287dd1b452db3aee1a9ab6ff5e2887

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/comments/dto/response/CommentsResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/comments/dto/response/CommentsResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebef77e13aaf39ba0e8be5df980a0173c64802ccaaa67358f8436dd46c034809
-size 938
+oid sha256:131b13a23a4e0f5bced8969c50477eb320b4870b23ffa816c93dee2a6550256b
+size 992
