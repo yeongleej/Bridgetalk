b85295618f2adce7e05b2d69a229a85aca709c24
조한빈 <whgksqls9999@naver.com>
2024-05-14 10:02:21 +0900

    [Design] icon
    Former-commit-id: bd9d9a25143544983e867cdc3e497e57edf2623f

--- bridgetalk-front/src/pages/parent/ui/parentInformationMain/parentInformationMain.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationMain/parentInformationMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79ed3e5bc62fce670166eed5922aa68c6980dc4e7cf0f537af532afd3b736806
-size 891
+oid sha256:25f701dcfece017007da07ebb1b6ab2dab2da0438842646215923c64a6d27f54
+size 1001
