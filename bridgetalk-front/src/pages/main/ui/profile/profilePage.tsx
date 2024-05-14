d37dccad42c8d7410299ada7aee9bbfe35887e95
조한빈 <whgksqls9999@naver.com>
2024-05-14 15:45:04 +0900

    [Feat] 설정 버튼 눌러서 부모 페이지 접근
    Former-commit-id: 246f87055b3c3bf35c15d7b40e197924446d0dcb

--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79c5580b6335dc5e09918aaf76e8504681d79f7ecb5e420ac1a4b255456e55c2
-size 5357
+oid sha256:b9f688e2217c9b6037f2cbae31e1e8ba31ab5f27d807383e52b162c0d2a1695f
+size 5182
