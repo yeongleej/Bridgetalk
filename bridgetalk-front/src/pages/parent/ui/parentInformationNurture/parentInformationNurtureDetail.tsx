02387db68717376e33ca7a2cebc50d31fca27e94
조한빈 <whgksqls9999@naver.com>
2024-05-14 09:36:24 +0900

    [Design] 육아정보 바인딩
    Former-commit-id: 7c7715c51ca2380882f9875973b9459be42fbea0

--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurtureDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurtureDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:887fc3d59d35862a8ffcf5914de5f8959e31e070905b2a6540b996aa07c245b6
-size 973
+oid sha256:f6be66bec10f74955cad31abf442f438d7a7390b31aa1cbd5a244b08842ea686
+size 1359
--- bridgetalk-front/src/styles/parent/parentInformationNurturDetail.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationNurturDetail.style.ts
@@ -1,8 +1,3 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  width: 80svw;
-  height: 70svh;
-
-  overflow-y: auto;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:7541c9fb7c0211f10436d6013db264bdadfd80ab1363408e70f58ab61146d363
+size 706
