088d301067398559a170ba0d59e5a0ffa09a0ade
sunkyoung <yskkjdh@gmail.com>
2024-05-15 06:28:33 +0900

    [Fix] 잘못된 매핑 수정 patch -> get
    Former-commit-id: 98560638271ddb9fc131e265c11b1eb488d9d299

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/controller/BoardsControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/controller/BoardsControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30743bc20adaa5c1f58b00fcc0eb3b28a27afc7a4f296a8bcece5c3be1f34a07
-size 23589
+oid sha256:0ef7d56e583ebf77889d3fb0385939f11e7da873ea372aa690b4e9500a33496f
+size 23601
