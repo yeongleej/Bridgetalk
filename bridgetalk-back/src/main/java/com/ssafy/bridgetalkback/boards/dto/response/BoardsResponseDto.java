0c64d4118a3973a85fecc8d996405a6195a0abf8
hyun0-25 <lisa1072@skku.edu>
2024-05-14 09:20:28 +0900

    [Refactor] 게시글 번역 과정에서 영어 추가
    Former-commit-id: 55e91896ed51adc7eea7f3f1dd7586fb20d0c016

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/dto/response/BoardsResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/dto/response/BoardsResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be55b180890aa401a4886b37dc7296395eccf42818a3aaaa8e857da471f3c650
-size 1202
+oid sha256:d97b4ddef44f6359c1b4f82538333f1b99c3616526cbbbb03dd64bb4c6ab3f3a
+size 1280
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb2cb268c5625865f812220524a8811ed1f99332d20ba559f956a2085ed6f53f
-size 4769
+oid sha256:07da4e0104275c409b0b25dd39dd13279256aea5388edddb7de53acc8f2f9a59
+size 5109
