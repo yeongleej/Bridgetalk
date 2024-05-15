e075ad11810d12099419e62154f89981ea7981ab
sunkyoung <yskkjdh@gmail.com>
2024-05-15 21:24:33 +0900

    [Refactor] 프로필 선택(로그인) 시 비밀번호 인증 하도록 수정 (부모)
    Former-commit-id: 6595b00abd824c0f6bdb16367cf7144659621d7e

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/controller/AuthController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1b2a59a7f1c8c2cefda2631adb3cdf092e247510d97eaffe7486518da2c11bf
-size 2797
+oid sha256:394aadc14f6a75bd8a451082be66091fa89adfad6775d6c5843c4012924cc72c
+size 2858
