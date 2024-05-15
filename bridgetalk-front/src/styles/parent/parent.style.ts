dcdcf9577cee04180d650eb6a97beb8ba934cd9a
조한빈 <whgksqls9999@naver.com>
2024-05-15 22:12:38 +0900

    [Feat] 부모 페이지 navbar
    Former-commit-id: e227c34eea8244f252145cb75874c93b8894ffbf

--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,30 +1,3 @@
-import styled from 'styled-components';
-
-export const Background = styled.div`
-  background-image: url('/assets/img/parent_bg.png');
-  background-size: cover;
-  background-position-x: 30%;
-  width: 100svw;
-  height: 100svh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  overflow: hidden;
-
-  .lang {
-    position: fixed;
-    top: 3svh;
-    right: 2svw;
-
-    background-color: transparent;
-    border: none;
-
-    cursor: pointer;
-
-    img {
-      width: 5svw;
-    }
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:e439feb15c28e0a920346c9570ccc63c9b844507e15af17db9cff928329a426e
+size 962
