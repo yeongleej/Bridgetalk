f93ea352a509df16adad33c61b5637d6c580b308
조한빈 <whgksqls9999@naver.com>
2024-05-16 03:29:32 +0900

    [Design] 부모 메인페이지 레이아웃 조정
    Former-commit-id: 38725f46b871450ca23e04fc7a7112ce822855d9

--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c8ab05b89bc6c06cffaf0ad2e1545ba3d7f0e997d57cff9cc57e28a56e67130
-size 1701
+oid sha256:7174535ab92543f17e05befadacd6d63eba738060fecb1d1a96fa45c446a4031
+size 1895
