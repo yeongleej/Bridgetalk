fd55a75ae82c257701dfe94c36ab8ad4fee8fb7a
조한빈 <whgksqls9999@naver.com>
2024-05-16 02:26:10 +0900

    [Design] 양육정보 레이아웃
    Former-commit-id: 2b370de55f39496cff7d0f5adefffad193bf1cd3

--- bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationNews.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:515dbe8258528ee4b5c33f3093221839db925ca4da7121a0a4cd87e58c5f642d
-size 2603
+oid sha256:d8301031fe5e0f1867048faa79bbb329544ea8d829532394fb02bde708aefa89
+size 2847
