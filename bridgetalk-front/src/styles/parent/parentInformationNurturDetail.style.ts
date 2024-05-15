172f9ca885093bb354e5c272675764a94ff5f186
조한빈 <whgksqls9999@naver.com>
2024-05-16 03:14:25 +0900

    [Design] 육아 정보 디테일 디자인
    Former-commit-id: 38902a4822f7f163b80f18afc050d55c8cd27ea7

--- bridgetalk-front/src/styles/parent/parentInformationNurturDetail.style.ts
+++ bridgetalk-front/src/styles/parent/parentInformationNurturDetail.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0386de7bfdfa513f265fe08e0654341ebfc6aff9b2ad16df8bb19f244a61078
-size 719
+oid sha256:b912d3d7a1d770e3d74ba0ab570b9f3e3703fdd85202c05f462b221ae81a7a79
+size 825
--- bridgetalk-front/src/styles/shared/backButton.style.ts
+++ bridgetalk-front/src/styles/shared/backButton.style.ts
@@ -1,18 +1,3 @@
-import styled from 'styled-components';
-
-export const Button = styled.button`
-    border: none;
-    background-color: transparent;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-
-    position: fixed;
-    top: 3svh;
-    left: 2.5svw;
-
-    img {
-        width: 3svw;
-        height: 3svw;
-    }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fdad2f1ac89a1fd7a13057bf9e7419f600b5a0066f7c192813808365a96bb3d
+size 285
