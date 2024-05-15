e5655caf229ae2656753316c97fa2760935538d0
조한빈 <whgksqls9999@naver.com>
2024-05-15 23:40:33 +0900

    [Design] 부모 페이지메인 레이아웃 조정
    Former-commit-id: f2ffaf3b8397373c3b50d962f32a92bfead76605

--- bridgetalk-front/src/styles/parent/parentReportList.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportList.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be97a37454cfaa4c37dba2503641f0b21d7eef76e0f3f97115881572b876ff15
-size 418
+oid sha256:e711d3f8863341f4c6c7433237f545dadb7ab66310111f3a902efeb4d678d37f
+size 438
