8b6226b37f93738a22c3c9619554e4db17ea100c
조한빈 <whgksqls9999@naver.com>
2024-05-16 02:28:53 +0900

    [Design] 양육정보 테두리
    Former-commit-id: d8c22528fab51ea1cb7eefc5b7fbb70d88639c40

--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c1560484e245e844951668d40917dbe9a6362f51e997d49bba7840b829247e
-size 1651
+oid sha256:2c8ab05b89bc6c06cffaf0ad2e1545ba3d7f0e997d57cff9cc57e28a56e67130
+size 1701
