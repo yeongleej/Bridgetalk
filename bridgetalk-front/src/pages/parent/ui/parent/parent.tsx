8a575dbc2879d0b0413cd36acf0bb403a43e0f21
조한빈 <whgksqls9999@naver.com>
2024-05-15 22:23:18 +0900

    [Design] 부모 페이지 Navbar 디자인 및 라우팅 연결
    Former-commit-id: 05bf20bd9d1a12f7296d0e8bd99d18cdb9035c2d

--- bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
+++ bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6933e57c0b4a2bacb4ce9d5a7b36457742812d2489552e1bd13ffcd68c31614
-size 1793
+oid sha256:0c3f4e416881506d75bd4547e37a8a98facf0734c9d0be4f0f978a365a943fbe
+size 2147
