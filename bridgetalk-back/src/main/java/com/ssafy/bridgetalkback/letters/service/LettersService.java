d4fe43a639458645e2b6c9876a98e3f220021aa6
방소영 <bjo8244@naver.com>
2024-05-16 10:11:03 +0900

    [Refactor] Reports에 해당하는 편지 1개만 생성하는 예외처리 추가
    Former-commit-id: a2f9bed4f15a5b81b4121dfac22fc0787e1412ab

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82919c1439d8f740c7cd000cb8fb770d6c372ed815684fa156f86351411affdd
-size 11084
+oid sha256:36a5b96137076c958b7f4aa1c3959ab6a94e5fe07de1ae8b587506cd8bdf6429
+size 11238
