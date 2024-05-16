9d9f107755d8b383af99348c4497a6e4526c1e66
조한빈 <whgksqls9999@naver.com>
2024-05-16 13:50:41 +0900

    [Feat] 공룡 스케일 변경
    Former-commit-id: 5996093705fd3c245750d419cf131a492d8da9e8

--- bridgetalk-front/src/pages/main/ui/profile/components/item/dinoSelect.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/components/item/dinoSelect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:962416ac1b1b613196562fc056e5f1ad526d2e472a6a5eeb7877a5f1ce9e3817
+oid sha256:44d4e58d2c59ae85364981f8b470caf35b6e7fbbbadf1751a853e4d6e5e5a53c
 size 3037
