11206722c1c1b0a977b0d67c8afea005394aceeb
조한빈 <whgksqls9999@naver.com>
2024-05-16 09:03:26 +0900

    [Design] 부모 페이지 게시판 디자인중
    Former-commit-id: 50d6c58456a67c74023c4ab240d6730a260ebfb9

--- bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListRight.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a275c20d941a8ce6c568281d7e7eace2568520a0b3db405e68b3656a4a59e907
-size 2577
+oid sha256:c6a0bc8dccf48a543e2c46da0a2cfde6eaccbc623ef3bdb118bea9474f1702de
+size 2574
