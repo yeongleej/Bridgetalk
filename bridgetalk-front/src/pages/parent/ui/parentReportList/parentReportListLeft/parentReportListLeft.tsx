dd19e8bcaa2f86be0f1cc5ffcd23cc5ed0bf2ea9
조한빈 <whgksqls9999@naver.com>
2024-05-16 12:58:01 +0900

    [Design] 리포트 리스트 페이지 게시판 디자인
    Former-commit-id: c436f8c49f35811f2952a2d174bf247f4c6ed68c

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aee44b499c5f9f00080f85b59c98e583050c14e8e2b4078d5cf6f367fde3d0c
-size 3416
+oid sha256:2c6c80f856b6dffbb892f170c8a3be0997f36785ea4965c1b9ac04393d6d2ae2
+size 3546
--- bridgetalk-front/src/styles/parent/parentReportListLeft.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListLeft.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e84db9da281ebca35618fd01135ea32d4cc07e4159e40dffe936782d1e0d01ef
-size 2685
+oid sha256:a76adc1f4ab54d48f1733b6a587149a5782ba3426686013cdd14c4c66b51c0b1
+size 3140
