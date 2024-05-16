5a18ce4e04ae5f6b5ad9cb20c9854aa3cdff5f07
박시연 <xion2664@gmail.com>
2024-05-16 16:30:38 +0900

    커뮤니티 완료
    Former-commit-id: ab5988dbd1273528f2b5a64568e5250117f154b2

--- bridgetalk-front/src/pages/main/query/postSignin/postSignin.ts
+++ bridgetalk-front/src/pages/main/query/postSignin/postSignin.ts
@@ -1,5 +1,3 @@
-import { customAxios } from '@/shared';
-
-export async function postSignin(signinDto: any) {
-  return customAxios.post(`/auth/login`, signinDto).catch((err) => console.log(err));
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2a74e3a0b1172817c3080b23eea79a7edc5e2a51758337da67a73cdff1d4c96
+size 180
---
+++ bridgetalk-front/src/pages/parent/ui/community/components/items/replyListItem.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4cc6a4f86c867b9ed039a364a2d1c9556b842f6a56abb1d78385785f9d176b9d
+size 1262
