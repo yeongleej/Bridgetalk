b6fed256c27380ce9e5afdbb88734dfb987bd8de
조한빈 <whgksqls9999@naver.com>
2024-05-16 16:05:30 +0900

    [Feat] 닉네임 중복 체크 API 연동
    Former-commit-id: e7d975f61363e8843b1c813409c8be34d904d9bc

--- bridgetalk-front/src/pages/main/model/handleNicknameCheck/handleNIcknameCheck.ts
+++ bridgetalk-front/src/pages/main/model/handleNicknameCheck/handleNIcknameCheck.ts
@@ -10,11 +10,8 @@ export async function handleNicknameCheck(nickname: string, setErrorModalState: 
     }
     return false;
   } catch (err) {
-    console.log(err);
     if (err instanceof Error) {
-      console.log(err);
-      errorCatch(err, setErrorModalState);
-      return false;
+      return errorCatch(err, setErrorModalState);
     }
   }
 }
--- bridgetalk-front/src/pages/main/query/getNicknameCheck/getNicknameCheck.ts
+++ bridgetalk-front/src/pages/main/query/getNicknameCheck/getNicknameCheck.ts
@@ -1,10 +1,7 @@
 import { customAxios } from '@/shared';
 
 export async function getNicknameCheck(nickname: string) {
-  console.log(nickname);
-
-  return customAxios.get(`/auth/nickname-duplicate/${nickname}`).catch((err) => {
-    console.log('에러 던지기');
+  return await customAxios.get(`/auth/nickname-duplicate/${nickname}`).catch((err) => {
     throw err;
   });
 }
