6be1978e32e676b80fb1e2b6b5ebe971fe864db1
조한빈 <whgksqls9999@naver.com>
2024-05-16 09:51:31 +0900

    [Feat] 프로필로그인 API 연동
    Former-commit-id: e83bf4d554d8ba8a62614885b7e6640acf1ae6e3

---
+++ bridgetalk-front/src/pages/main/model/handleProfileLogin/handleProfileLogin.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab9abbd34b6598c6861448960866ab253fc8874c24b00ffe06facee439222869
+size 638
--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9f688e2217c9b6037f2cbae31e1e8ba31ab5f27d807383e52b162c0d2a1695f
-size 5182
+oid sha256:780796d2803af1116591402ecea5006fd2f1bd27da88d2826c8b18196b55959e
+size 4998
