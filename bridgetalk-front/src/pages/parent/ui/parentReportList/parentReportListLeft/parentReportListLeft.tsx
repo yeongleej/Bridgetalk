75e9265881d6d305392481b8ee845e22e89330db
조한빈 <whgksqls9999@naver.com>
2024-05-16 09:03:26 +0900

    [Design] 부모 페이지 게시판 디자인중
    Former-commit-id: df77ee49d0496b1ad12a24e7197f979943fc93c3

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
@@ -1,5 +1,3 @@
-import * as S from '@/styles/parent/parentReportListLeft.style';
-
-export function ParentReportListLeft() {
-  return <S.Container></S.Container>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0b574d1affb9e996111c1678b0c087601b945262eefb781ecfa9c47c4bd3c9c
+size 1797
--- bridgetalk-front/src/styles/parent/parentReportListLeft.style.ts
+++ bridgetalk-front/src/styles/parent/parentReportListLeft.style.ts
@@ -1,10 +1,3 @@
-import styled from 'styled-components';
-import { color } from './common.style';
-
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-
-  width: 100%;
-  height: 100%;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:52d4eb73ef437ab5ddc934c316f13a0d14c96400b5d9667fc0f7569dd8d717ee
+size 771
