a7537b7c506e4f487992efe237771fbbbb0c8d44
이지영 <abcd23589534@gmail.com>
2024-05-16 13:21:44 +0900

    [Feat] 알림보내기 로직 추가
    Former-commit-id: eaea3bdea16e04fcfbe1b8c3bb2b40141bd5941a

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/service/LettersService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82919c1439d8f740c7cd000cb8fb770d6c372ed815684fa156f86351411affdd
-size 11084
+oid sha256:fdb9df0d9eb0ca6f6e4ee545c69ca116233338e6fee7fa04a96acf65dbfa4d31
+size 11948
