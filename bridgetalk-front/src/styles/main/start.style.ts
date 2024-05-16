cc99d9e14963d5e4ac23b8e23e32719e6099f3ed
조한빈 <whgksqls9999@naver.com>
2024-05-17 04:32:27 +0900

    [Design] 배경 이미지 움직임
    Former-commit-id: e4fd0232c7db6e41974b98533a1dd589a2fcbfe4

--- bridgetalk-front/src/styles/main/start.style.ts
+++ bridgetalk-front/src/styles/main/start.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2b986fc0de38df4cc1ce15be00486b6cced67a3c60d03e6a702909bce49c1a9
-size 1390
+oid sha256:9e4598bb82c7f702b297e4b6c605992a841b30238792bf329ab79fe8340c7fc0
+size 1609
