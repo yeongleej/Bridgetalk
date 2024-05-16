f22799cb407008785749443301099211accdefef
sunkyoung <yskkjdh@gmail.com>
2024-05-16 12:34:32 +0900

    [Refactor] 닉네임 중복 검사 API 토큰 필요 x -> 시큐리티 인증 url에서 제외 처리
    Former-commit-id: 1f7b55db55c17f0f177f6ec75aa9136666fe36e2

--- bridgetalk-front/src/shared/enums/emotion/emotion.ts
+++ bridgetalk-front/src/shared/enums/emotion/emotion.ts
@@ -1,5 +1,3 @@
-const 긍정 = '긍정';
-const 부정 = '부정';
-const 행복 = '행복';
-const 슬픔 = '슬픔';
-const 화남 = '화남';
+version https://git-lfs.github.com/spec/v1
+oid sha256:3b98f23dfcb0544292ca8046c43a344ee32ae6629b3006648e698a9dec063f4a
+size 125
