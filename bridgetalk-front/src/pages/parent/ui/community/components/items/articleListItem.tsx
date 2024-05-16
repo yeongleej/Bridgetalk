6870e2b9710cd6088cc876c59ddd0ff7e6c438f4
박시연 <xion2664@gmail.com>
2024-05-16 14:37:26 +0900

    fix
    Former-commit-id: 55716039d706bdccf76d3e9632a67dee08a44d3e

--- bridgetalk-front/src/pages/parent/ui/community/components/items/articleListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/components/items/articleListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63426b87a8162e3ce247421996131bcde7fd22dffe70568bfe3ed02c92b2f46a
-size 1267
+oid sha256:31ca2c013bad57ce9f0efd6b1da23e0edc1a91f715ea5c62226bf7ecb7619c3d
+size 981
