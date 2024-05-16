8b241cdf09a2c61db05b5165cb43c5ba8c3aa3de
조한빈 <whgksqls9999@naver.com>
2024-05-16 11:21:43 +0900

    [Design] 리포트 옆 게시판 디자인중
    Former-commit-id: 46f0e388dccf701e7e11a0eb69cfc7051385d8c9

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0b574d1affb9e996111c1678b0c087601b945262eefb781ecfa9c47c4bd3c9c
-size 1797
+oid sha256:6aee44b499c5f9f00080f85b59c98e583050c14e8e2b4078d5cf6f367fde3d0c
+size 3416
--- bridgetalk-front/src/shared/util/index.ts
+++ bridgetalk-front/src/shared/util/index.ts
@@ -1,2 +1,3 @@
 export * from './cookie';
 export * from './customAxios';
+export * from './dateToString';
