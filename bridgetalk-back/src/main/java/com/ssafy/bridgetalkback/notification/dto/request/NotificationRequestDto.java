5a57c45af5817cf54a94b5415a751d0cd0d03a64
이지영 <abcd23589534@gmail.com>
2024-05-16 13:21:44 +0900

    [Feat] 알림보내기 로직 추가
    Former-commit-id: 607996c290b81679a1bff8e8c4035f0565129841

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/notification/dto/request/NotificationRequestDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/notification/dto/request/NotificationRequestDto.java
@@ -1,13 +1,3 @@
-package com.ssafy.bridgetalkback.notification.dto.request;
-
-import com.ssafy.bridgetalkback.notification.domain.NotificationType;
-import lombok.Builder;
-
-@Builder
-public record NotificationRequestDto(
-        String receiverUuid,
-        String content,
-        String url,
-        NotificationType notificationType
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:02247436ea1e118332ad7285de9f2e2c606d41f308429765c14d1a711a776a07
+size 322
--- bridgetalk-front/src/pages/main/query/postProfileLogin/postProfileLogin.ts
+++ bridgetalk-front/src/pages/main/query/postProfileLogin/postProfileLogin.ts
@@ -1,5 +1,3 @@
-import { customAxios } from '@/shared';
-
-export async function postProfileLogin(UUID: string) {
-  return customAxios.post(`/auth/profileLogin/${UUID}`).catch((err) => err);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0b7dae3082d6175989dcb09c3e4a96383998b019e00a02946526267665791ff
+size 175
