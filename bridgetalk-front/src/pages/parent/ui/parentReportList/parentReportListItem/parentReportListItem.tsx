22bb794f47157b74887fff6cc9b9a3f41aa4deb0
조한빈 <whgksqls9999@naver.com>
2024-05-16 13:43:36 +0900

    [Fix] 오류 수정
    Former-commit-id: 0de805647d02a511be0a2b15f0e33e973161223d

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c14894688550a7e4bdfe83591da589732d875ad7f8712f3da3263c5bd496874
-size 2042
+oid sha256:5a08f63dd99609fa6f24147831b6e5d994d37240155d9470c1e3b5a240ddadeb
+size 1967
