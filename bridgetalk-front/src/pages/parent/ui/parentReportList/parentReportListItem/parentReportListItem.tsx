e39e5817e05eec5a10215c2e227469a545752ae1
조한빈 <whgksqls9999@naver.com>
2024-05-16 13:02:08 +0900

    [Design] 리포트 리스트 DINO 구현중
    Former-commit-id: 96f082b1b13bf9738c5a9d2ee1b30f4757b6068f

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f8c6cbc56a01e3cb4c10eecdd41f7c1bccd8542cdb6faa92adc2bcb7e8fa43b
-size 1787
+oid sha256:0c14894688550a7e4bdfe83591da589732d875ad7f8712f3da3263c5bd496874
+size 2042
