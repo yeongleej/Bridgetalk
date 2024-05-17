97acf87f6304e8cccd8cd4e331551ecf4fb06a6c
이지영 <abcd23589534@gmail.com>
2024-05-17 10:00:32 +0900

    [Feat] test code 버그 수정 및 게시글 좋아요 등록 알림 추가
    Former-commit-id: b3c7a73b860bce00c7cecce5b39d7a98bc1587ac

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/ClovaSpeechServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/ClovaSpeechServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1084c12c45674283e3f10a816c069173a721fb18dd5b732c3c25e1be827a3ffe
-size 1485
+oid sha256:839fbd8059e18117d7439ff7206fa9a8cf008477f677a2bf676716b3658ad1f8
+size 1490
