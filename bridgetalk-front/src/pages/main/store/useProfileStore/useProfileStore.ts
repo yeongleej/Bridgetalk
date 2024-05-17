6c7b19223cec2b7a3425de756e9fa7979fd77efb
조한빈 <whgksqls9999@naver.com>
2024-05-17 13:56:52 +0900

    [Fix] 아이 프로필 변경 시 비밀번호 체크 로직 추가
    Former-commit-id: 4a7e400dc9aa707b54aeffddea6d440e24116fab

--- bridgetalk-front/src/pages/main/store/useProfileStore/useProfileStore.ts
+++ bridgetalk-front/src/pages/main/store/useProfileStore/useProfileStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e4bf9ad74ad7637e56c34c3d89731ff14bbaf2224b2c2f55383681ea4253425
-size 517
+oid sha256:4fe342f2126f6f198a939aeab7989f510391070af3120211bd458279b74937fa
+size 695
--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a42cbdfd7be558c919716afa2597d8af13d2e6383a2e8167b7077945e10b2067
-size 5340
+oid sha256:a675b7cc01989ad15b77d5e8762fbe8a830988ba22c8898029e8647846c2d854
+size 5446
