1a597da20f8b0be5b9dda5d7a89cf13b1f394710
박시연 <xion2664@gmail.com>
2024-05-17 10:32:56 +0900

    dressing(ing) + welcomeScreen
    Former-commit-id: 87e00298dc8a72b57eee5cb92ce3fe899560f226

---
+++ bridgetalk-front/src/pages/Test/ui/testCamera/utils/drawDress.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7660911d9802b61eadd655e7d363177c4fe387f7573391d97483e9db3c4a03d6
+size 2101
--- bridgetalk-front/src/pages/Test/ui/testCharacter/testCharacter.tsx
+++ bridgetalk-front/src/pages/Test/ui/testCharacter/testCharacter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85559a3c8a1bab273f8c1535e5b6daa0868b665bb4d00b26a310734db201ab0f
-size 1680
+oid sha256:4c26b3ae0878d770c60eb9795ec6f9a0fdd1e6a4e282ba8b3a14c4181b09e234
+size 2920
