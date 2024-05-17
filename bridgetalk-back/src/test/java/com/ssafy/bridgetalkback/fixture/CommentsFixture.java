71c6385be21d14b71a0dc2995de664e4f3ca17ca
sunkyoung <yskkjdh@gmail.com>
2024-05-17 13:01:37 +0900

    [Test] 게시글, 답글 필리핀어 버전 저장으로 인한 테스트코드 수정
    Former-commit-id: a527c5112091a436737b36756b3695640c2778c0

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/fixture/CommentsFixture.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/fixture/CommentsFixture.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8f04e36732f8e3e8ce436a96046bb0d1dae06d85b19e73a6bccba85036ba6d9
-size 737
+oid sha256:9d9ecf266d25e6bcbb34c58b3df678bc097fb2055c0be60bcdb8b2ff1ed99854
+size 836
