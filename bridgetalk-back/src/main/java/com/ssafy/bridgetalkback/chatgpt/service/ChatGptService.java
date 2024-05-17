d3e9559d48487d6d886ea4487c62294a8c320a71
이지영 <abcd23589534@gmail.com>
2024-05-17 17:56:30 +0900

    [Feat] 필리핀어 번역추가  완료
    Former-commit-id: 37468ef8a235db2ed2d2c17fff98af6bd2a8a88a

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56d74609160e2edd62b341ceb456691144eaf4e5ca41ab6dd573f6e68634ae76
-size 9390
+oid sha256:c12dbcae807d73f38206a963856d13588f875711c0f0e5e3a1f40642557e82b5
+size 9384
