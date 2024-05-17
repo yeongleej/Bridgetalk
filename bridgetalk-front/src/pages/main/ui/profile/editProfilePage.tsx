06dcc58fbe5024709d2dae0cdfc6118da77fda0d
조한빈 <whgksqls9999@naver.com>
2024-05-18 00:40:06 +0900

    [Feat] 아이 프로필 추가 테스트
    Former-commit-id: 9dde9b685d41db1fb51e8478359d6ccd94715560

--- bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/editProfilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ce2ed8ec69c17159948b442980ba5104680e93d1ef55d2bd6e20a443f3e8541
-size 10380
+oid sha256:ca690425fa3a54c2c2e835cdab37b5918544e88ce524e97276099dcddb27f184
+size 10626
