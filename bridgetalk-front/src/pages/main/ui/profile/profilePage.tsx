fb04f0b29e9fe7f2da12322fd84a38f693bf916c
조한빈 <whgksqls9999@naver.com>
2024-05-17 10:03:36 +0900

    [Design] 프로필 리스트 프로필 선택 방식 변경
    Former-commit-id: e209f0eeb593623a0dbd576e76197f57185b7c9c

--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c78e5f18f1b2f6311118e86f0a333120dcfadeb6b887499333f19903d9ff94bf
-size 5189
+oid sha256:b54dd3043d3f3a5987bdd565002082135741a76e3d263b08c34b1d9928ecb34f
+size 5312
