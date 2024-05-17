96ab9f3fc099071b9f2a1aeaf2f9511ea27dcaa2
조한빈 <whgksqls9999@naver.com>
2024-05-17 16:18:50 +0900

    [Design] 부모 페이지 미비점 수정
    Former-commit-id: 849dc64d07bea2580bb065a0482ddb6418f3378d

--- bridgetalk-front/src/shared/ui/modalSpace/modalSpace.tsx
+++ bridgetalk-front/src/shared/ui/modalSpace/modalSpace.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e468ab3a8768a25d227536c90fd9c500fbda0c0b323ee25cc223a434c05dcbd7
-size 11114
+oid sha256:b3eb342a511458e486350d8d82f3bd31986bcbc7c2063bcb07cae6102ca7f6bb
+size 11273
