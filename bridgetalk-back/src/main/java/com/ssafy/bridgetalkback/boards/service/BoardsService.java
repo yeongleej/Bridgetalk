c2cb05858daef548536e8b46093a246a30f0ae00
sunkyoung <yskkjdh@gmail.com>
2024-05-17 23:35:18 +0900

    [Refactor] 불필요한 import문 제거
    Former-commit-id: 89b63ad6b52687bd19c8e582cc591eb488b1143b

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45149d05d13622bb1ea6ec7013c697244b7fd7c8d1b67810c313cd53a7b8021b
-size 9283
+oid sha256:19cfd60299dbbbbc8038ea9ac738e8b7dc4a9784f17dd36ac0801eb347242906
+size 9213
