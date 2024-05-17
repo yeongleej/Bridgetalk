8014f9484821e02133c84e106d88e62e76f53d22
박시연 <xion2664@gmail.com>
2024-05-18 00:55:48 +0900

    asdf
    Former-commit-id: e98d21f39a8605ea4c06f31b47ff24b3b6901587

--- bridgetalk-front/src/pages/Test/ui/testCamera/utils/drawParts.ts
+++ bridgetalk-front/src/pages/Test/ui/testCamera/utils/drawParts.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e06e26e55339f8618957b232eec8b00518c68a96c6943ae9192ffe917bed2a8
-size 4272
+oid sha256:eb2c28e266672a5e8a26d672fa1c0ac3d645f934bc41066f25e00b59fbdb30de
+size 4219
