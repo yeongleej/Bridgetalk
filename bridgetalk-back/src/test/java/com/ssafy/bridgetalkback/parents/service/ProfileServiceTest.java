8e28784b182acf7d93548b5df8a8ff2f57d052a7
sunkyoung <yskkjdh@gmail.com>
2024-05-17 17:49:38 +0900

    [Test] 회원탈퇴 api 호출 시 password 인증 시 통과 구현으로 인한 테스트코드 수정
    Former-commit-id: 90e71a26bc1569d37847798f41fdf2eac810b626

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parents/service/ProfileServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parents/service/ProfileServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c69c3ae399fb6ec80e7d5264a70dc34a178ff6f46ec0404cd3e0c37f597a363
-size 4875
+oid sha256:6a697b5ad6c731076739d40de2a22be0a617532cce7f860d521d45f54d877667
+size 5359
