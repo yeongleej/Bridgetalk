0f36a40d0cab5cddb0b4d7c410d8e81cd036ba54
조한빈 <whgksqls9999@naver.com>
2024-05-17 10:11:48 +0900

    [Design] 부모 페이지 Nav 아이콘 수정
    Former-commit-id: b907d7d876ea1a4a85bae9737c9a62bee8b871d4

--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7174535ab92543f17e05befadacd6d63eba738060fecb1d1a96fa45c446a4031
-size 1895
+oid sha256:8913e9df263c1d3d37482d550e38fe052f2e69060489636890affec17a35d47a
+size 1896
