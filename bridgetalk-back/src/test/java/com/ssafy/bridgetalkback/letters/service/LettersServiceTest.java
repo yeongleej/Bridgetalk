683d230ca0710bf915a5c08701a60a776d7281b3
이지영 <abcd23589534@gmail.com>
2024-05-17 10:00:32 +0900

    [Feat] test code 버그 수정 및 게시글 좋아요 등록 알림 추가
    Former-commit-id: a4ec73ba07d5da398dbf91248de46cd3f9d95446

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/LettersServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/letters/service/LettersServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f79b6cd80227e50b8bde5240421e45f03abe3fe5a008a4d7bf08771143cbe69d
-size 7835
+oid sha256:c90027da8a0b3e10405492eb06eb71387a73f5336102d5594d2298429b1c10fa
+size 7822
