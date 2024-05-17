b28b36b6ab56f41ac504ea18ca31bcad9763a2a3
이지영 <abcd23589534@gmail.com>
2024-05-17 16:38:11 +0900

    [Feat] 필리핀어 키워드 변경 제외하고 완성
    Former-commit-id: 4450b231f5265c9cd660cdcd6729a7c7bb167d76

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7391ab3cc1604887c3bec9708b04331375f1be7a7111d3890fc545852d6cc54e
-size 5884
+oid sha256:b8924303b824946fb982b48736f78eb2157dea3801cce63acf5d09af23e3cafe
+size 5950
