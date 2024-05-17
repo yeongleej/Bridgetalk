bdfedc27a0ae94cc5f01496f3255799dadb2ae4d
sunkyoung <yskkjdh@gmail.com>
2024-05-17 16:28:09 +0900

    [Test] Comments 리스트 조회 API 관련 Service, Controller 테스트코드 작성
    Former-commit-id: 016e6f214e7531b12312fbb6928732e07484a151

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/Language.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/global/Language.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:22a27b1ab4edd53e70aaf2d909862e51fca3cf8a9a9fc56fad8bf6f86a49e8df
-size 85
+package com.ssafy.bridgetalkback.global;
+
+public enum Language {
+    kor, viet, ph;
+}
