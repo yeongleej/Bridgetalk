6005f6826ed76bb469076317d822206afb3618a2
조한빈 <whgksqls9999@naver.com>
2024-05-17 13:42:09 +0900

    [Fix] navigate 의존성 제거
    Former-commit-id: a24d799f634edc1916ff46f837a5c7c10b21a42a

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/ParentsSignupRequestDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/ParentsSignupRequestDto.java
@@ -1,10 +1,3 @@
-package com.ssafy.bridgetalkback.auth.dto.request;
-
-public record ParentsSignupRequestDto(
-        String parentsEmail,
-        String parentsPassword,
-        String parentsName,
-        String parentsNickname,
-        String parentsDino
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:541805511efd3f140a5d4194816c63e0ecdeef3a2329610bef1f8f0dfab60fd7
+size 245
