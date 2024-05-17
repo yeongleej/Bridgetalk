cd37909eb0c5b26e42caa5badffd447d6bf04c16
sunkyoung <yskkjdh@gmail.com>
2024-05-17 17:49:20 +0900

    [Refactor] 회원탈퇴 api 호출 시 password 인증 시 통과되도록 수정
    Former-commit-id: 545fb6105980b67547c98574f610f7a462033c78

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/service/ProfileService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parents/service/ProfileService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5e5eba83e604fd7544449bb05e8da9928d71d349297fd8b1f1a32a73a7f46aa
-size 3541
+oid sha256:ddc900f27e60c939d45b97b286f7bbcafb7c6d4cfc5c6f6dcb06a5b8cc86ef65
+size 4355
