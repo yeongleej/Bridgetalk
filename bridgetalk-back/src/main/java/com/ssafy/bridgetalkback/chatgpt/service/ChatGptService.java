eae0273bea6498dfe4ded28e489b18902dae311f
이지영 <abcd23589534@gmail.com>
2024-05-17 17:11:31 +0900

    [Hotfix] stt chatpt 호출 명령어 수정(한국어로 번역하도록 수정)
    Former-commit-id: c094da91cf2e09138b323f288bdd04a30093ea83

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2270b25b401a078313e39167750b15874c12fee12705775d64457b8cdbcf09d4
-size 9155
+oid sha256:fcba4d054ff617e23314ffcee3071c577efd329aab263a1d3e05ec1f16de2bd2
+size 9182
