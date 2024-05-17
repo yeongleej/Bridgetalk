9c6e67bb3f0ce0a937cbec63f860b91f7b3286f9
sunkyoung <yskkjdh@gmail.com>
2024-05-17 23:33:56 +0900

    [Refactor] 게시글 상세조회, 리스트조회 필리핀어 버전 제공하도록 수정
    Former-commit-id: 6e0326c3b87ac28f84025b1d7d03d480672365a9

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/service/BoardsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a76653aaf6dae72b9cb6de487cc542dcf6a3be8685b7c15e6aca9202f22dde31
-size 9338
+oid sha256:45149d05d13622bb1ea6ec7013c697244b7fd7c8d1b67810c313cd53a7b8021b
+size 9283
