a227f44a6b75727ddb29071f4da0b77009db6acb
방소영 <bjo8244@naver.com>
2024-05-17 16:27:05 +0900

    [Design] 커뮤니티 리스트 디자인
    Former-commit-id: 49a7da1fb9842b7e63ad7bd51fc8574ebbd181f8

--- bridgetalk-front/src/app/appRoutes.tsx
+++ bridgetalk-front/src/app/appRoutes.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c5b8d9e60f89c6f740d217e88b89ce691093beca6fa538aec4ca35830e5f999
-size 5293
+oid sha256:1df1a301dd5655b5600ea53e38b281ae9e85434f9b16895ab7fc288f9c65250a
+size 5289
--- bridgetalk-front/src/pages/parent/ui/community/components/articleList.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/components/articleList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b837c0384beb4699455d097ffe9843780fd6aa8df86149041596556e052f91f6
-size 906
+oid sha256:83e98f31b97906a524255225b5d0d36a6fbbf340bff69291eb83ef5d275c0e5e
+size 1674
