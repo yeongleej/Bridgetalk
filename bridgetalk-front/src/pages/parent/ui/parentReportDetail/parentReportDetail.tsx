636a88fdfc0a338e6f95f7daeb8c288e609186c6
조한빈 <whgksqls9999@naver.com>
2024-05-18 08:50:21 +0900

    [Feat] 부모 페이지 필리핀어 추가
    Former-commit-id: 92ebfb2bc1c18a767efe584a3c5293dce40e667a

--- bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportDetail/parentReportDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ce0b9e31e49fb9db8deb37513b4802e1eef184da154fd2c01459eda1bdf01b
-size 3834
+oid sha256:fc9ae0c4bf3773e624ced1dd717b9d44be76ecb083a79fb323b628d87b325f3d
+size 3846
