72f3df5f72b04d479a73c639c87956e62c426de8
sunkyoung <yskkjdh@gmail.com>
2024-05-17 16:25:49 +0900

    [Test] 사용자 인증 없이 게시글 조회가능하도록 수정된 것에 맞춰 테스트코드 수정
    Former-commit-id: 45f92b5a0e44c5962a19f8604014ad10abaf6b30

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsListServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsListServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a6e2e344316527b03ddbca5002cdd57180fca253ee8e8eb256b1d87bf671c23
-size 10016
+oid sha256:5d6481822eb0a930a11998c877e179ed0049fe78966a5c0ef854de0c3449c4f8
+size 9466
