e54dbfbe96112ec760acae7ab8ddf4971179fe3b
조한빈 <whgksqls9999@naver.com>
2024-05-17 09:40:30 +0900

    [Design] 프로필 리스트 출력 오류 수정
    Former-commit-id: e9abd5d8609cb8015e9d8fb1452b649ac3a130b0

--- bridgetalk-front/src/styles/main/inputEmail.style.ts
+++ bridgetalk-front/src/styles/main/inputEmail.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73db970c76cb8f97e6f21488c4f1e6e09beafa242fd3ddaab24e4a07e33c65db
-size 1989
+oid sha256:73515a61b334851addb277ced855d8dc432894c7d5915f15b5259956321bc3cd
+size 2011
