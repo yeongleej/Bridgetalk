9c0b16bc19e298e8b9a5ba518429bf360d88ef96
조한빈 <whgksqls9999@naver.com>
2024-05-17 10:07:15 +0900

    [Refactor] 프로필 리스트 출력 방식 변경
    Former-commit-id: 03a0396d510342f28bf4b6164b1aa27ea00939db

--- bridgetalk-front/src/pages/main/model/handleNicknameCheck/handleNIcknameCheck.ts
+++ bridgetalk-front/src/pages/main/model/handleNicknameCheck/handleNIcknameCheck.ts
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5bbdbca24050033ea5b8a11faa71b7ede8e33200936025edbc2e9baee4e71d79
-size 428
+import { errorCatch } from '@/shared';
+import { getNicknameCheck } from '../../query';
+
+export async function handleNicknameCheck(nickname: string, setErrorModalState: any) {
+  try {
+    const response = await getNicknameCheck(nickname);
+
+    if (response.status == 200) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    if (err instanceof Error) {
+      return errorCatch(err, setErrorModalState);
+    }
+  }
+}
