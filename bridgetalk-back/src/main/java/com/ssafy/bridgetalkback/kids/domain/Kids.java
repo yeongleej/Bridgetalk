af735c7af32211e813c822ee344e98bc05ab3910
sunkyoung <yskkjdh@gmail.com>
2024-05-18 00:08:15 +0900

    [Refactor] kids 엔티티 kidsPassword 컬럼 추가 및 프로필 추가 service 수정
    Former-commit-id: 2570c9561623faa13c97e8a7c87936f96d887211

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kids/domain/Kids.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/kids/domain/Kids.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c005c2314387af8a60b794780cd038dad73649b196c9e6a46f45dbb808d057f
-size 1983
+oid sha256:4afc330b21c036c08e790f3fe7c3b105ff4c434746c346cbd5a99667be385490
+size 2198
