e7309492d3d256b1ac562af142d3510724aab99b
조한빈 <whgksqls9999@naver.com>
2024-05-18 09:19:40 +0900

    [Fix] 서비스 워커 fetch 에러 잡는중
    Former-commit-id: 202c5a13e5fef4676e14bd3c013f38b79fc66ba7

--- bridgetalk-front/public/serviceWorker.js
+++ bridgetalk-front/public/serviceWorker.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55602c1db8db96d8c631b0f50dfad95e40d2824846d6ead1316da92d820ab076
-size 1330
+oid sha256:934670009eb4df9b09cce0454c970a2ca3cbf43feff12b4382a478741c0a60be
+size 1679
