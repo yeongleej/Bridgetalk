45f2edb682d83568425480ec378989131e84e8ae
sunkyoung <yskkjdh@gmail.com>
2024-05-19 00:33:56 +0900

    [Test] 육아정보 필리핀어 버전 지원에 따른 테스트코드 수정
    Former-commit-id: ef55650c8e00cc36fef6023de6cced374a9f4e60

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/controller/ParentingInfoControllerTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/controller/ParentingInfoControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d329f4fa939e82e9b050ed4b9f02d31a39b8001dcb9bebffd73208c6128237f
-size 9112
+oid sha256:eca4c08950ed9629617ab53ad5ea2ed767b4048fe02e045cfd6d176689bd1f6f
+size 9104
--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoListServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/parentingInfo/service/ParentingInfoListServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74dd5bdfa0e01e1809b89b4f368ddb8b8d306367a3b3cfeb92b80fba06bc954a
-size 6290
+oid sha256:23cabbddb553aec47d3a4aa0f25a07119215c1fe47725c509bf431d9767359bf
+size 8030
