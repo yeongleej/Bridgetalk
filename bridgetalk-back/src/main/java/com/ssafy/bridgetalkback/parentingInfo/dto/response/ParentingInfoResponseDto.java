1b7ec369d51262b2f4908a06f95f498dde585627
sunkyoung <yskkjdh@gmail.com>
2024-05-19 00:24:49 +0900

    [Feat] 육아정보 필리핀어 버전에 대한 상세조회 및 리스트조회 기능 추가
    Former-commit-id: 2496b3e3dc2f02744c0b1bfa43dff680c238d38d

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/dto/response/ParentingInfoResponseDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/dto/response/ParentingInfoResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86c858d6383b779d1baea7c9d7c9b0308be409b8d9796f94031953ad447081ea
-size 981
+oid sha256:ebf19159d75292bd98463716eae0e53d3a34376ab89818d800dddb01d5f7e618
+size 1609
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/query/ParentingInfoQueryRepositoryImpl.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/query/ParentingInfoQueryRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37ae6bb7a429875b5fb94080c2e76305743a8e61e93c45548368fe060f8872f8
-size 3054
+oid sha256:703f92544a3e19c75a15ea3dd10df6618e6b68a2a9b54a903bda01ad624560e3
+size 3280
