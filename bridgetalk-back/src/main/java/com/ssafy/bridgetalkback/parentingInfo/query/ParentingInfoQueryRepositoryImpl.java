3727ca2ab42b7c16817b9889a6a06e7cfbf47242
sunkyoung <yskkjdh@gmail.com>
2024-05-19 01:22:51 +0900

    [Refactor] 육아정보 4개 기준으로 페이징하도록 수정
    Former-commit-id: 7873f72c376bd7836a1103c0a306377a0e4f0835

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/query/ParentingInfoQueryRepositoryImpl.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/query/ParentingInfoQueryRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:703f92544a3e19c75a15ea3dd10df6618e6b68a2a9b54a903bda01ad624560e3
+oid sha256:45de3d7a618380f3bfe39bb101e4fb92576f7c3d37681b0d29f6a461634e690f
 size 3280
