67fc7bb5415da32358f75847804584640cf5ddca
sunkyoung <yskkjdh@gmail.com>
2024-05-19 15:32:19 +0900

    [Test] Boards 및 Comments 좋아요 여부 확인 API 관련 Service, Controller 테스트코드 작성
    Former-commit-id: 0e3db6208ad8283a9ca3ceaef6710578249f4cf6

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsLikeServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsLikeServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5813693fb9b2c0dd8be7a43680058353d13e97a6fa7ee4964c05f33febe24be2
-size 5164
+oid sha256:ceb54de6be8d7157a0f66091c78540382f39556830064610ee1a15314f1d1ec3
+size 5897
