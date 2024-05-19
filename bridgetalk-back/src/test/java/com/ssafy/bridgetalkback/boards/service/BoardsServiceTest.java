7b2f0ee8a75f706fec935133706682a8d6972776
sunkyoung <yskkjdh@gmail.com>
2024-05-19 15:18:49 +0900

    [Refactor] BoardsResponseDto writer -> parentsName로 변수명 변경에 따른 테스트코드 수정
    Former-commit-id: bce00ae72cb1dce77a55518893093a58e41393e6

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/boards/service/BoardsServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15f09e03ddafecdf55663da63a15b0eef11bfc0e1c9f447336fa5b3d73057ea3
-size 15255
+oid sha256:71330370bcbaab50750d3f7d72715bf8857f870cc67332dc588f4a6b8be6a6ea
+size 15282
