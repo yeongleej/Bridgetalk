9481dae7511b9639122149e255a20168e7795eee
조한빈 <whgksqls9999@naver.com>
2024-05-19 16:18:21 +0900

    [Design] 게시글 작성 화면 레이아웃 조정중
    Former-commit-id: 728057368df54aee884b9cc25e0bbe893496823a

--- bridgetalk-front/src/styles/parent/createPage.style.ts
+++ bridgetalk-front/src/styles/parent/createPage.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d263708f8119c8053a3ec8dc8fb57d733a52e81fe647d1e57913c8e2b3fe3afc
-size 1409
+oid sha256:e0e31f817174058e1939b86576bf9dc7038da0fb2f722a36b010ca1aeab83b37
+size 1428
