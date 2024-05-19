6089f4e8815684db76f190860d5a28cbf6ca364f
sunkyoung <yskkjdh@gmail.com>
2024-05-19 15:19:15 +0900

    [Feat] Boards 좋아요 여부 확인 API 구현
    Former-commit-id: 4afeca64c4bf56765887a468153b00eb0e38c714

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b514587228485e90e7e1498e27c5ac653914790ee459a053c89f5bcac0374d71
-size 6546
+oid sha256:2db0c74b5d03df8595c6d3b2deba51a1f0e96a1305ff6404957392374d62e01f
+size 6833
