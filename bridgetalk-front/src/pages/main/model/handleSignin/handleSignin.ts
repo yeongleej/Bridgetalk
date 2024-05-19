e819c8d095bbcd5efaf7b62921e1def66e0b09ed
조한빈 <whgksqls9999@naver.com>
2024-05-20 05:25:21 +0900

    [Feat] 초기 선택한 국적에 따라 언어 변경 옵션
    Former-commit-id: 9a951fa819cf7da7c9a38b64ac3e7782a50d7b9b

--- bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
+++ bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a59918a9468e83364cc3238c1c9fd469ae43f0d8f280dd8586fd656106f9996a
-size 1654
+oid sha256:fcc8f5a473135cb61920fb0f4a4388fddecce13e283d99b5f32c2f5b195b65fc
+size 1712
