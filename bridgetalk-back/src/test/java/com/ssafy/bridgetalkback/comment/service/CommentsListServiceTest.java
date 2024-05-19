74fb94da633f8c81b0663376c0142c64f99261a0
sunkyoung <yskkjdh@gmail.com>
2024-05-19 13:04:30 +0900

    [Test] 답글 전체 리스트 조회 -> 특정 게시글에 대한 답글 리스트 조회로 수정에 따른 테스트코드 수정
    Former-commit-id: d5e822e47a6cf0690538ae2cc6fc01e36b8fc89a

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/comment/service/CommentsListServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/comment/service/CommentsListServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d32e7e0eee89da103862209ee13664d0b959557fb31a332c468114216dcbe2e5
-size 9175
+oid sha256:ba9a4e6ccd8c0923b676edea3b8c21bc5c448904926f92a883cacc093993d8ef
+size 9260
