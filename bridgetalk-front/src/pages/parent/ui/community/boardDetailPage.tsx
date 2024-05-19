f20249ae363031f6cb224d41819ee08c7eb8353b
조한빈 <whgksqls9999@naver.com>
2024-05-20 03:55:06 +0900

    [Design] 게시글 작성폼 디자인
    Former-commit-id: 545b311999b73568f1454bba3631b92772e4bce2

--- bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:276c2d4e495a78e81856020de7178c4de0b461507834a6cff8b66b7997335166
-size 3620
+oid sha256:81fec713e84d1ade6371c2f6f4ddf56c9b2e29ecb0b575c2c32227d4ab43dee2
+size 3678
--- bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4e5322601accfafa6b07b66cce0f3fb6def3770d530d34a56487b7b40fa1514
-size 5461
+oid sha256:ec35fdefa1cf1353a0fbbc3c04b7923d5fbccd01f4aed9172d1e1150373e5ef8
+size 5419
