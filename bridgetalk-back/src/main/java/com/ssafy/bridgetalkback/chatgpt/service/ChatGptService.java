4803d7eff590d7837c6c873710ce349151b703a8
hyun0-25 <lisa1072@skku.edu>
2024-05-19 15:03:12 +0900

    [Refactor] 레포트 생성시 베트남어, 필리핀어 동시 번역으로 수정
    Former-commit-id: 75e3f7d84ce20425262b4f6ef136c0851ad286da

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/chatgpt/service/ChatGptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50922db6a3fed9b0f50e8ba85f680713a6accebb172d2889060239b62a4fa837
-size 9608
+oid sha256:388212d1791d2215cf23d10c04c8cf2821ff06a496e0be80d449342eede313b3
+size 11838
