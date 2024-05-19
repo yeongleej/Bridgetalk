108bb3c6f3a4228f78beaee46c20e73733e35837
조한빈 <whgksqls9999@naver.com>
2024-05-19 13:12:41 +0900

    [Fix] 게시글 페이지네이션 버그 픽스
    Former-commit-id: 960b9ec24eadae533a2399e293bac9a0ffdb4f38

--- bridgetalk-front/src/pages/parent/ui/community/boardPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/boardPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc694736c22d139af019f30211a6a5b390b6c534eb541d1019b96a0efd25b929
-size 2754
+oid sha256:1454df43f79aac29796f7b6015f984ca9ec6376e8a5d21faea7e5ba8a6fddcce
+size 2811
