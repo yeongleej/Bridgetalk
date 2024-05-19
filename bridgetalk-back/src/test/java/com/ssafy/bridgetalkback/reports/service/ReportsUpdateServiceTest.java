d93e06c8662b0b70add097a732d060a0f6daf8dd
hyun0-25 <lisa1072@skku.edu>
2024-05-19 15:03:23 +0900

    [Test] 동시번역 관련 테스트코드 수정
    Former-commit-id: 54520d4cedbe003766165d9397ac0e22af418574

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/ReportsUpdateServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/ReportsUpdateServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d040e378bc01819c2ebce797b33a5ce546ac0846f1f899adcd88c47aa6011f80
-size 4849
+oid sha256:c574e97a836adaed743c127cafa176a7cb2a67ac7b47e0b18fee269ccdc74c75
+size 7295
