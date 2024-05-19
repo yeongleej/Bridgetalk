79cc2e1329eab770fa54460365290d4d9621b434
sunkyoung <yskkjdh@gmail.com>
2024-05-19 11:43:55 +0900

    [Feat] 이메일 인증번호 전송 및 검증 API 구현
    Former-commit-id: b0724bc97ab7a342f7214cb1509a0641295a7d86

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/EmailAuthRequestDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1dba5cb58230105ab85bcab922ce9a51ea4356c8ee5dacbefc422aa3c3e1951
+size 447
---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/EmailRequestDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:02bcb5506468a47e2cba913ce62ce019b44058f04729f549e2219e700e1e489f
+size 347
