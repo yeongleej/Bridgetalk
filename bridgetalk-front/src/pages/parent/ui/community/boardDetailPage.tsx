c452887ee64e0e78fc17832af5ce8993eab4369c
조한빈 <whgksqls9999@naver.com>
2024-05-20 08:45:07 +0900

    [Feat] 게시글 좋아요
    Former-commit-id: d7a82c20f443a9d2d9c7c9d00a3cdb63f1a85d28

--- bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81fec713e84d1ade6371c2f6f4ddf56c9b2e29ecb0b575c2c32227d4ab43dee2
-size 3678
+oid sha256:30857e7bf55f6cdfd4c0f60ca314949fecff8ea7e4123f40183b8f0a21a4b9fc
+size 4840
