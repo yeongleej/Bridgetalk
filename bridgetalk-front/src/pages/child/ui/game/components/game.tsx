fc6072dd09207e8aab37ef94881cd9fba66090ff
박시연 <xion2664@gmail.com>
2024-05-20 01:47:27 +0900

    [Feat] Puzzle nation
    Former-commit-id: 48c82087e062bbaeec51e9875ed990bf41adc73d

--- bridgetalk-front/src/pages/child/ui/game/components/game.tsx
+++ bridgetalk-front/src/pages/child/ui/game/components/game.tsx
@@ -1,3 +1,3 @@
-export function Game() {
-  
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c31e3a91ae1e2a389691b1c3bca03ca1198f7f8ff45ac203acc901ba77a11cf7
+size 31
--- bridgetalk-front/src/styles/child/game/stage.style.ts
+++ bridgetalk-front/src/styles/child/game/stage.style.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc690f39ada945895237c94b51c4dc05a0074f89bc16f302638439c819586711
-size 1030
+oid sha256:fd6fe3cc5189d5de1d0aa064dd1a77b02deecf6cd10efdad71c8cfd2d942f9fb
+size 1116
