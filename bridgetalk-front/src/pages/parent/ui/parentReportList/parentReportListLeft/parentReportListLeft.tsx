7a765b0af8c8fc74b1ad75ba1a6002de07dd9045
조한빈 <whgksqls9999@naver.com>
2024-05-20 05:03:32 +0900

    [Feat] 리포트 댓글 가져오기
    Former-commit-id: 3a1fd0352ccbd43c849148206480577385cbd9af

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListLeft/parentReportListLeft.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1294925155bc134e89334c994225fb02b4f696fcc70c21d55838c4b4e6d05ee
-size 2745
+oid sha256:5a0f0e4bfbba809e4d10223e464c60cc08498179c486a7f2d27bab62b6d77bce
+size 2742
