87637d8d89456108c831193c33c1160da4057cf0
조한빈 <whgksqls9999@naver.com>
2024-05-19 13:10:04 +0900

    [Feat] 게시글 등록 후 리스트 페이지로 이동
    Former-commit-id: 8ead4a99fc35ca8569c8df980a18739eaf5d88f6

--- bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f62d819a53654d2f5af1121cdc45d1f9c381b1f4563ba419718193c640caf472
-size 4453
+oid sha256:e82ebcb9050346e0196da89e6ee0b7c1431f3a48ba73cb6e9429aa10fcd28b1f
+size 4860
