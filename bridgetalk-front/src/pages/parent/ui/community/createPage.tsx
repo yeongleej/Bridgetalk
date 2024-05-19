3bfa51480d4c36f4ba42b2c62e99d5dde16bce29
조한빈 <whgksqls9999@naver.com>
2024-05-19 15:50:38 +0900

    [Fix] 게시글 렌더링 수정
    Former-commit-id: 23d93918f6f24adc64bd685e3be056d2a83601a2

--- bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e82ebcb9050346e0196da89e6ee0b7c1431f3a48ba73cb6e9429aa10fcd28b1f
-size 4860
+oid sha256:988441b4e0cbe660c0553d28e87cba1491e5eac2f0c0ba0793f9ed9374775485
+size 4932
