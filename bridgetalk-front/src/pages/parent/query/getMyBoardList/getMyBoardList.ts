7741371c704c1200c8f5bb776698152c4ac5ee04
조한빈 <whgksqls9999@naver.com>
2024-05-20 04:29:07 +0900

    [Feat] 내 게시글 불러오기
    Former-commit-id: ecc66f85d623f442a7e93b6d0f6aa9d7f484e38d

--- bridgetalk-front/src/pages/parent/query/getMyBoardList/getMyBoardList.ts
+++ bridgetalk-front/src/pages/parent/query/getMyBoardList/getMyBoardList.ts
@@ -1,13 +1,3 @@
-import { customAxios } from '@/shared';
-
-export async function getMyBoardList(language: any, sort?: string) {
-  return customAxios
-    .get(`/boiards/my/${language}`, {
-      params: {
-        sort,
-      },
-    })
-    .catch((err) => {
-      throw err;
-    });
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ebb308523e67c54edb0deb473aa8cb595011654b6c5ace2b8ec43a2fac13229
+size 263
