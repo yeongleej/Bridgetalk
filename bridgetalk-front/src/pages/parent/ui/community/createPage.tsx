f05f43642022bd3ceb88964337292257aa03f2b7
조한빈 <whgksqls9999@naver.com>
2024-05-20 12:19:35 +0900

    [Feat] 커뮤니티 베트남어 추가 번역
    Former-commit-id: 06e3b16706121a45361209f74a1e45e229b8c2c5

--- bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/createPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:499371ccb159370d1c122ef9b7e390fa420a9381f825a51b277e6582a581f929
-size 5425
+oid sha256:b92dd30c0d8c7ee7207cd09478a75c2d991b6f1ada53bf89f6924dffd26fcb5e
+size 6259
