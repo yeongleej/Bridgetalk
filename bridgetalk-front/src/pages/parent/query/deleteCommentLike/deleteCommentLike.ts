1aa4dca3324bef04c10ee56a02c0d829afd072f1
조한빈 <whgksqls9999@naver.com>
2024-05-20 09:06:24 +0900

    [Design] 댓글 좋아요
    Former-commit-id: 776d118d0d837563672fd9ad32a3bac8801b8173

--- bridgetalk-front/src/pages/parent/query/deleteCommentLike/deleteCommentLike.ts
+++ bridgetalk-front/src/pages/parent/query/deleteCommentLike/deleteCommentLike.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34bb22cda747426d3f24984e6d3273195e055a70f4ceb5c7da3c4991da025222
-size 203
+oid sha256:1aa8ec7ed1a1daae4833d05b50292e5d5a57acf2902f2bbd0c0a6c5a40e6e6b9
+size 204
--- bridgetalk-front/src/pages/parent/ui/community/components/items/replyListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/components/items/replyListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59f87aad0d00a0c61bbad47159c7168ce79b6cfb86730f02493d4f2c0fcc7de1
-size 2279
+oid sha256:0ae7b1af098921a2ff21376ec9c3cee999820cbdea5d358b9d7183edd9d0f63f
+size 2347
