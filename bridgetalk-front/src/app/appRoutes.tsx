4be210e061f41b9b438522a1a92aebff2941fe4d
조한빈 <whgksqls9999@naver.com>
2024-05-20 14:41:56 +0900

    [Fix] 빌드 에러 해결 - 테스트 파일 주석처리
    Former-commit-id: 50d0ecb9d4900b4ba924474851492b3ff36945f2

--- bridgetalk-front/src/app/appRoutes.tsx
+++ bridgetalk-front/src/app/appRoutes.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af6707bf6fd65cc9616127364e6825a4328c9cbbbff588ef0c5da00488ce3aba
-size 5472
+oid sha256:c45f2506b3eb5fd6f4bb2814cbec3c0432eff8bdb6211b92ed5b3b2fe00fc08e
+size 5494
