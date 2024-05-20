3398476783bd24ea5df16d2765c1a823fd583c61
조한빈 <whgksqls9999@naver.com>
2024-05-20 09:28:03 +0900

    [Feat] 이메일 인증
    Former-commit-id: cff0d6510671a6c9d116c79c5c181af3edeb71cd

---
+++ bridgetalk-front/src/pages/main/query/postAuthSend/postAuthSend.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea8bfb96448e38419c2acdee2c8c52be4de5aa386284c85d7ff5d6537e70fc7c
+size 220
--- bridgetalk-front/src/pages/main/ui/sign/components/inputEmail.tsx
+++ bridgetalk-front/src/pages/main/ui/sign/components/inputEmail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aed5d0f60b9e0602946a979fc4baadaf8c5503181ae51b04ae036c442aaa436
-size 1523
+oid sha256:e8bc07d608618dcf7d2fa986eb8a0de15fc9ac72734e9801f2ba3f8444083cd5
+size 2437
