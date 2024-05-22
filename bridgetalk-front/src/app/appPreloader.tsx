8263274c608945828d7412a40ed8cbaaf08c630f
조한빈 <whgksqls9999@naver.com>
2024-05-22 14:52:22 +0900

    [Refactor] 프리로드 이미지 에셋만 적용
    Former-commit-id: 819effa7b4d8db4d8a84b43c6b2c6ae17c8015b0

--- bridgetalk-front/src/app/appPreloader.tsx
+++ bridgetalk-front/src/app/appPreloader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05981c5b6ca91cdbe0cf3a32e16071e3f765e3efda3783bcc7bbbf4ea586c54c
-size 1202
+oid sha256:0c685c1ae1489aa842cfa6b7ffaab125d57ae8fcb6ab4d001611f32616ef39e0
+size 1125
--- bridgetalk-front/src/shared/util/preload.ts
+++ bridgetalk-front/src/shared/util/preload.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ac0ab96282c953a03d4ddb4d7b9598d5fd8b82093d66298a084289b47316b35
-size 529
+oid sha256:7fbb7a103be71a8def3a5ea1c646ebfe78dcc2eee1ed08c3b6e82b858f93008b
+size 433
