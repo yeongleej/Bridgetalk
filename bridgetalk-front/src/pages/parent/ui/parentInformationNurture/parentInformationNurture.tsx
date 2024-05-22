af5c8c6316c2cd606eab9559b2f91e833ea3db53
조한빈 <whgksqls9999@naver.com>
2024-05-22 14:34:54 +0900

    [Refactor] 양육정보 key값 적용
    Former-commit-id: d62128138abc4c0e2ffda9fbf3a9fa4968e2716b

--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7464810fc1ee0847cec55c3de5ae623ce8b4c91ff58ee2e34f0ebb7d90b0f018
-size 5021
+oid sha256:9880c035f377399c51c730a04f06ff1d7b18669f4eb47b2059791eeee8a8e669
+size 5077
