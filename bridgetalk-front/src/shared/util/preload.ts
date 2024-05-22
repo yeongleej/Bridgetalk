4808574ede199caea693829cc5504d2e0e85c60e
조한빈 <whgksqls9999@naver.com>
2024-05-22 14:20:10 +0900

    [Refactor] 프리로드 오류로 기능 임시 해제
    Former-commit-id: a29bc200adba76f4bc7417b6c7196d594580dc7e

--- bridgetalk-front/src/shared/util/preload.ts
+++ bridgetalk-front/src/shared/util/preload.ts
@@ -1,9 +1,3 @@
-export const importAll = (r: __WebpackModuleApi.RequireContext) => {
-  return r.keys().map(r);
-};
-
-// 특정 디렉토리의 모든 이미지 파일 가져오기
-export const imageUrls: any = importAll(require.context('../../../public/assets', true, /\.(png|jpe?g|svg)$/));
-
-// 특정 디렉토리의 모든 GLB 파일 가져오기
-export const glbUrls: any = importAll(require.context('../../../public/assets', true, /\.glb$/));
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ac0ab96282c953a03d4ddb4d7b9598d5fd8b82093d66298a084289b47316b35
+size 529
