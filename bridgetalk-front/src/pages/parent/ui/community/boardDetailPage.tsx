48c7e3432246623c8fd75245284213389da9a703
조한빈 <whgksqls9999@naver.com>
2024-05-22 14:48:28 +0900

    [Refactor] 게시판 key값 적용
    Former-commit-id: 7aae25231a543b4b65089415df2a02bc2c06862f

--- bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/boardDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e963a4aba6461b36ea5307aca110abc2ee36f84742235d31def317104e4c48c3
-size 6276
+oid sha256:bd5b128f376c91da9fa4c0222445b5455ec83d62dc034e7b79a871ceb9c69391
+size 6299
