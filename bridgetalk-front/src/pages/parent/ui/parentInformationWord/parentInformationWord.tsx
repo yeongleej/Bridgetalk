92caf991c340539ef08ffcf595a3830636b069e5
조한빈 <whgksqls9999@naver.com>
2024-05-22 14:36:43 +0900

    [Refactor] 단어 정보 key값 적용
    Former-commit-id: 4d0a3f57934870ad9dc773d013c3a566e450950a

--- bridgetalk-front/src/pages/parent/ui/parentInformationWord/parentInformationWord.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationWord/parentInformationWord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12df430f56bddca561f10e08f781148e25afb75c83b3da872382a467e777500e
-size 3035
+oid sha256:2a8e6be2751a7d45832bb6c5d006de11a0dbdd8ccdfa69f44fec26d42d2e0c07
+size 3065
